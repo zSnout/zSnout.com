8e77e021b51e501933f9d7853881b4ee1c7bd5d0
Zachary <zsakowitz@zsnout.com>
2022-06-02 14:10:37 -0700

    Add LogoWithName

---
+++ src/components/LogoWithName.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  import { useDark } from "@vueuse/core";
+  import LogoWithNameDark from "./LogoWithNameDark.vue";
+  import LogoWithNameLight from "./LogoWithNameLight.vue";
+
+  const isDark = useDark();
+  LogoWithNameDark;
+  LogoWithNameLight;
+
+  withDefaults(defineProps<{ invert?: boolean }>(), { invert: false });
+</script>
+
+<template>
+  <LogoWithNameDark v-if="invert !== isDark" />
+  <LogoWithNameLight v-else />
+</template>
