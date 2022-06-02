e1968a82df83abab6b14606cd4123f98370d9de2
Zachary <zsakowitz@zsnout.com>
2022-06-02 08:29:25 -0700

    Add DocumentDisplay

---
+++ src/components/DocumentDisplay.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+  import Navigation from "./Navigation.vue";
+  import SafeAreaLR from "./SafeAreaLR.vue";
+</script>
+
+<template>
+  <Navigation :floating="false" />
+
+  <div class="safe-area">
+    <SafeAreaLR>
+      <slot />
+    </SafeAreaLR>
+  </div>
+</template>
