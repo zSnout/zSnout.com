2b70f463dbfbea19db154b26f34cee91a16202e8
Zachary <zsakowitz@zsnout.com>
2022-06-02 08:41:54 -0700

    Make `Home` use `DocumentDisplay`

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+  import DocumentDisplay from "../components/DocumentDisplay.vue";
+</script>
+
 <template>
-  <h1>Welcome to zSnout. We're working on adding things...</h1>
+  <DocumentDisplay>
+    <h1>Welcome to zSnout. We're working on adding things...</h1>
+  </DocumentDisplay>
 </template>
