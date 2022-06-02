c90b27c2302d181a48d068141790724abdf38f66
Zachary <zsakowitz@zsnout.com>
2022-06-02 08:40:44 -0700

    Add SafeAreaLR

---
+++ src/components/SafeAreaLR.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+  import { UseScreenSafeArea } from "@vueuse/components";
+</script>
+
+<template>
+  <UseScreenSafeArea left right>
+    <div class="padding">
+      <div class="content">
+        <slot />
+      </div>
+    </div>
+  </UseScreenSafeArea>
+</template>
+
+<style lang="scss" scoped>
+  .padding {
+    display: flex;
+    justify-content: center;
+    padding: 0 12px;
+  }
+
+  .content {
+    width: min(1000px, 100%);
+  }
+</style>
