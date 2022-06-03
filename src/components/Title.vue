3f90138e04c6e8eaa266a646c9a1c942ac1cbfe3
Zachary <zsakowitz@zsnout.com>
2022-06-03 13:43:39 -0700

    Add Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="title">
+    <slot />
+  </h1>
+</template>
+
+<style scoped>
+  .title {
+    padding: 0.5em;
+    padding-top: 0;
+    border-bottom: 2px solid currentColor;
+  }
+</style>
