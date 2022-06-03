50b1ca96953432927e34c789ef55f7e397550291
Zachary <zsakowitz@zsnout.com>
2022-06-02 18:06:36 -0700

    Retain page height when using DocumentDisplay

--- src/components/DocumentDisplay.vue
+++ src/components/DocumentDisplay.vue
@@ -1,14 +1,41 @@
 <script lang="ts" setup>
   import Navigation from "./Navigation.vue";
-  import SafeAreaLR from "./SafeAreaLR.vue";
+  import SafeArea from "./SafeArea.vue";
 </script>
 
 <template>
   <Navigation :floating="false" />
 
   <div class="safe-area">
-    <SafeAreaLR bottom>
+    <SafeArea :class="$style.document" bottom keep-height top>
       <slot />
-    </SafeAreaLR>
+    </SafeArea>
   </div>
 </template>
+
+<style lang="scss">
+  #app {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
+
+<style lang="scss" scoped>
+  .safe-area {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+  }
+</style>
+
+<style lang="scss" module>
+  .document > {
+    :first-child {
+      margin-top: 0;
+    }
+
+    :last-child {
+      margin-bottom: 0;
+    }
+  }
+</style>
