d091cc335f0123457717ed10725e77ca3f4d48e7
Zachary <zsakowitz@zsnout.com>
2022-06-04 07:02:08 -0700

    Add Field component

---
+++ src/components/Field.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+  import { ref, watch } from "vue";
+
+  const { modelValue } = defineProps<{
+    modelValue: string;
+    type?: string;
+    autocomplete?: string;
+    placeholder?: string;
+  }>();
+
+  const emit = defineEmits<{
+    (event: "update:modelValue", modelValue: string): void;
+  }>();
+
+  const value = ref(modelValue);
+  watch(value, (val) => emit("update:modelValue", val));
+</script>
+
+<template>
+  <input
+    v-model="value"
+    class="second-layer focusline"
+    :autocomplete="autocomplete ?? 'off'"
+    :placeholder="placeholder"
+    :type="type ?? 'text'"
+    style="display: block"
+  />
+</template>
