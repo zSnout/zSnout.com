6dbfdc4445a7b58537054be83f68f80779b81168
Zachary <zsakowitz@zsnout.com>
2022-06-03 17:00:37 -0700

    Add minimum height for SafeArea and `center` option

--- src/components/DocumentDisplay.vue
+++ src/components/DocumentDisplay.vue
@@ -1,12 +1,33 @@
 <script lang="ts" setup>
+  import { MaybeElement, useResizeObserver } from "@vueuse/core";
+  import { ref } from "vue";
   import Navigation from "./Navigation.vue";
   import SafeArea from "./SafeArea.vue";
+
+  defineProps<{ center?: boolean }>();
+
+  const navbar = ref<MaybeElement>();
+  const navHeight = ref(58);
+
+  useResizeObserver(
+    navbar,
+    (entries) => {
+      const height = entries[0].borderBoxSize?.[0].blockSize;
+      if (typeof height === "number") navHeight.value = height;
+    },
+    { box: "border-box" }
+  );
 </script>
 
 <template>
-  <Navigation :floating="false" />
+  <Navigation ref="navbar" :floating="false" />
 
-  <SafeArea bottom top>
+  <SafeArea
+    :flex="center"
+    :min-height="`calc(100vh - ${navHeight}px)`"
+    bottom
+    top
+  >
     <slot />
   </SafeArea>
 </template>
