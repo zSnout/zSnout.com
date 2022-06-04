b189e71d596ae67a552dbaa547f4a36453a5fb07
Zachary <zsakowitz@zsnout.com>
2022-06-03 21:48:54 -0700

    Add `SecondLayer` component

---
+++ src/components/SecondLayer.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+  import { onMounted, useCssModule, useSlots } from "vue";
+
+  onMounted(() => {
+    const slot = useSlots().default;
+
+    if (slot) {
+      for (let el of slot()) {
+        el.el?.classList.add(useCssModule()["second-layer"]);
+      }
+    }
+  });
+</script>
+
+<template>
+  <slot />
+</template>
+
+<style lang="scss" module>
+  .second-layer {
+    padding: 0.5rem 0.75rem;
+    background-color: white;
+    border-radius: 0.5rem;
+    box-shadow: 0 3px 6px 1px #ddd;
+
+    :global(.dark) & {
+      background-color: #2a2a2c;
+      box-shadow: 0 3px 6px 1px #1a1a1a;
+    }
+  }
+</style>
