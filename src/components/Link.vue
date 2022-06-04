fff73b075afc6d3fc5bb72cd19e07331afe1e348
Zachary <zsakowitz@zsnout.com>
2022-06-04 04:47:33 -0700

    Add `Link` to encapsulate cross-origin linking

---
+++ src/components/Link.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  import { RouterLink } from "vue-router";
+
+  const { to } = defineProps<{ to: string }>();
+  const isRelative = /^\/(?!\/)|\.\.?\//.test(to);
+</script>
+
+<template>
+  <RouterLink v-if="isRelative" :to="to">
+    <slot />
+  </RouterLink>
+
+  <a :href="to">
+    <slot />
+  </a>
+</template>
