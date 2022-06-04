4c5ebb82a3ae056518147c83064607397944f414
Zachary <zsakowitz@zsnout.com>
2022-06-04 08:24:11 -0700

    Add Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="button hoverline second-layer">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+  .button {
+    display: block;
+    font-size: inherit;
+    cursor: pointer;
+  }
+</style>
