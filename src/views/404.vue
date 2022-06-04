1876f6ef79b01d14014813b34908b4988b5292bc
Zachary <zsakowitz@zsnout.com>
2022-06-03 17:00:49 -0700

    Improve 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,9 +1,17 @@
 <script lang="ts" setup>
+  import { RouterLink } from "vue-router";
+  import DocumentDisplay from "../components/DocumentDisplay.vue";
   import { useTitle } from "../composables/useTitle";
 
   useTitle("Oops, 404");
 </script>
 
 <template>
-  <p>Oops, 404! Guess we haven't made this yet...</p>
+  <DocumentDisplay center>
+    <h1>Oops, 404! Guess we haven't made this yet...</h1>
+    <p>
+      How about
+      <RouterLink replace to="/">going back home?</RouterLink>
+    </p>
+  </DocumentDisplay>
 </template>
