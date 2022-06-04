3d1e9188252172f1f11ee4798308a05a491f7ef1
Zachary <zsakowitz@zsnout.com>
2022-06-04 06:06:02 -0700

    Make #app inherit background color of body

--- src/App.vue
+++ src/App.vue
@@ -55,6 +55,7 @@
     font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
       Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
       sans-serif;
+    background-color: inherit;
   }
 
   a {
