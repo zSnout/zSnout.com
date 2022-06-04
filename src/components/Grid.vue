edc165f10396ee5d53e05af157136543833ae339
Zachary <zsakowitz@zsnout.com>
2022-06-04 06:12:56 -0700

    Add Grid layout

---
+++ src/components/Grid.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(min(12em, 100%), 1fr));
+    gap: 0.75rem;
+  }
+</style>
