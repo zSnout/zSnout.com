f3f0f3d7af73dfcde677f1d7c41f03bb50a1f6c1
Zachary <zsakowitz@zsnout.com>
2022-06-05 18:15:58 -0700

    Add useNumericLike composable

--- src/composables/useNumericLike.ts
+++ src/composables/useNumericLike.ts
@@ -1,17 +1,35 @@
 import { MaybeRef } from "@vueuse/core";
-import { computed, ComputedRef, unref } from "vue";
+import { computed, ComputedRef, isRef, Ref, unref } from "vue";
 
 export function useNumericLike(
-  value?: MaybeRef<number | string | bigint | null | undefined>
-) {
-  return computed(() => {
-    let val = unref(value);
+  value?: Ref<NumericLike>
+): ComputedRef<number | undefined>;
+export function useNumericLike(value?: NumericLike): number | undefined;
+export function useNumericLike(
+  value?: MaybeRef<NumericLike>
+): number | undefined;
+export function useNumericLike(value?: MaybeRef<NumericLike>) {
+  if (isRef(value)) {
+    return computed(() => {
+      let val = unref(value);
 
-    if (val === null || val === undefined) return undefined;
-    if (typeof val === "string") val = parseFloat(val);
-    if (typeof val === "bigint") val = Number(val);
-    if (!Number.isFinite(val)) return undefined;
+      if (val === null || val === undefined) return undefined;
+      if (typeof val === "string") val = parseFloat(val);
+      if (typeof val === "bigint") val = Number(val);
+      if (!Number.isFinite(val)) return undefined;
 
-    return val;
-  });
+      return val;
+    });
+  }
+
+  let val = unref(value);
+
+  if (val === null || val === undefined) return undefined;
+  if (typeof val === "string") val = parseFloat(val);
+  if (typeof val === "bigint") val = Number(val);
+  if (!Number.isFinite(val)) return undefined;
+
+  return val;
 }
+
+export type NumericLike = number | string | bigint | null | undefined;
