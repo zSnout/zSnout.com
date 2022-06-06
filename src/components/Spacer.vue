565014d1f01b2289e977c2d6a2b47f7011fb8edc
Zachary <zsakowitz@zsnout.com>
2022-06-05 18:15:44 -0700

    Allow spacers to include multiple instances

--- src/components/Spacer.vue
+++ src/components/Spacer.vue
@@ -1,5 +1,11 @@
+<script lang="ts" setup>
+  import { NumericLike, useNumericLike } from "../composables/useNumericLike";
+
+  const { size } = defineProps<{ size?: NumericLike }>();
+</script>
+
 <template>
-  <div class="spacer" />
+  <div v-for=" in useNumericLike(size) ?? 1" class="spacer" />
 </template>
 
 <style scoped>
