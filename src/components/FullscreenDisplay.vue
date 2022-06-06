8dc67fe2d434346864fc73cb05af1544083d7a79
Zachary <zsakowitz@zsnout.com>
2022-06-06 00:07:38 -0700

    Add FullscreenDisplay

---
+++ src/components/FullscreenDisplay.vue
@@ -0,0 +1,107 @@
+<script lang="ts" setup>
+  import { UseScreenSafeArea } from "@vueuse/components";
+  import { RouterLink } from "vue-router";
+  import LogoDark from "./LogoDark.vue";
+  import SafeArea from "./SafeArea.vue";
+  import Modal from "./Modal.vue";
+  import { ref } from "vue";
+  import OptionsIcon from "./OptionsIcon.vue";
+  import Button from "./Button.vue";
+  import HStack from "./HStack.vue";
+  import Spacer from "./Spacer.vue";
+
+  defineProps<{ options?: boolean }>();
+
+  const open = ref(false);
+</script>
+
+<template>
+  <UseScreenSafeArea class="nav" top>
+    <SafeArea>
+      <HStack class="padding">
+        <RouterLink to="/">
+          <LogoDark class="logo" invert />
+        </RouterLink>
+
+        <Spacer />
+
+        <OptionsIcon class="options" role="button" @click="open = !open" />
+      </HStack>
+    </SafeArea>
+  </UseScreenSafeArea>
+
+  <Modal :open="open">
+    <slot name="options" />
+
+    <template #buttons>
+      <Button cancel @click="open = !open">OK</Button>
+    </template>
+  </Modal>
+
+  <div class="content">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .nav {
+    position: fixed;
+    top: 0;
+    z-index: 3;
+    width: 100vw;
+  }
+
+  .padding {
+    padding-top: 0.75em;
+
+    @media screen and (max-width: 400px) {
+      padding-top: 0.5em;
+    }
+  }
+
+  .logo {
+    box-sizing: content-box;
+    height: 2em;
+    filter: drop-shadow(1px 1px 2px #0008);
+
+    @media screen and (max-width: 400px) {
+      height: 1.5em;
+    }
+
+    .hover &:hover {
+      cursor: pointer;
+      filter: drop-shadow(1px 1px 2px #fffc);
+    }
+
+    :deep(g > path:not(:first-child)) {
+      stroke: white;
+      stroke-width: 0.2em;
+      paint-order: stroke fill markers;
+    }
+  }
+
+  .options {
+    height: 2em;
+    cursor: pointer;
+    filter: drop-shadow(1px 1px 2px #0008);
+    transition: var(--transitions), transform 0.3s;
+    fill: white;
+
+    @media screen and (max-width: 400px) {
+      height: 1.5em;
+    }
+
+    .hover &:hover {
+      transform: rotate(45deg);
+    }
+  }
+
+  .content {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
