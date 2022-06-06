11232153b0aad09b948a6d581e0cd5e5f23c2718
Zachary <zsakowitz@zsnout.com>
2022-06-06 00:01:16 -0700

    Add Labeled component

---
+++ src/components/Labeled.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+  import HStack from "./HStack.vue";
+
+  defineProps<{ label: string }>();
+</script>
+
+<template>
+  <label class="label">
+    <HStack>
+      <span class="nowrap">{{ label }}</span>
+
+      <slot />
+    </HStack>
+  </label>
+</template>
+
+<style lang="scss" scoped>
+  .label {
+    display: block;
+
+    > div {
+      align-items: center;
+    }
+  }
+
+  .nowrap {
+    white-space: nowrap;
+  }
+</style>
