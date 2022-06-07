8e9209164ddd7622c8449da7c0db772424751e37
Zachary <zsakowitz@zsnout.com>
2022-06-06 19:11:49 -0700

    Add onDispose function

--- src/composables/useCanvas.ts
+++ src/composables/useCanvas.ts
@@ -1,4 +1,9 @@
-import { MaybeElementRef, unrefElement, useElementSize } from "@vueuse/core";
+import {
+  MaybeElementRef,
+  tryOnScopeDispose,
+  unrefElement,
+  useElementSize,
+} from "@vueuse/core";
 import { Ref, watchEffect } from "vue";
 
 export interface CanvasSize {
@@ -9,27 +14,35 @@ export interface CanvasSize {
 export interface CanvasInfo {
   canvas: HTMLCanvasElement;
   size: CanvasSize;
+  onDispose(hook: () => void): void;
   onResize(hook: () => void): void;
 }
 
 export function useCanvas(canvas: MaybeElementRef) {
+  const onDispose: (() => void)[] = [];
+  tryOnScopeDispose(() => onDispose.forEach((hook) => hook()));
+
   return new Promise<CanvasInfo>((resolve) => {
-    watchEffect(() => {
+    const stop = watchEffect(() => {
       const el = unrefElement(canvas);
 
       if (el instanceof HTMLCanvasElement) {
         const { width, height } = useElementSize(el);
 
         const onResize: (() => void)[] = [];
+        if (typeof stop === "function") onDispose.push(stop);
 
-        watchEffect(() => {
+        const resizer = watchEffect(() => {
           el.width = width.value;
           el.height = height.value;
           onResize.forEach((hook) => hook());
         });
 
+        onDispose.push(resizer);
+
         resolve({
           canvas: el,
+          onDispose: (hook) => onDispose.push(hook),
           onResize: (hook) => onResize.push(hook),
           size: { width, height },
         });
