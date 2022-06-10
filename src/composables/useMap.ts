06a7d057b24a283bed5e1647fc6e67c890467b64
Zachary <zsakowitz@zsnout.com>
2022-06-10 08:15:07 -0700

    Add useMap

--- src/composables/useMap.ts
+++ src/composables/useMap.ts
@@ -1,3 +1,6 @@
+import { MaybeRef } from "@vueuse/core";
+import { computed, unref } from "vue";
+
 export function map(
   inputStart: number,
   inputEnd: number,
@@ -5,15 +8,26 @@ export function map(
   outputEnd: number,
   value: number
 ) {
-  inputStart = inputStart;
-  inputEnd = inputEnd;
-  outputStart = outputStart;
-  outputEnd = outputEnd;
-  value = value;
-
   return (
     ((value - inputStart) * (outputEnd - outputStart)) /
       (inputEnd - inputStart) +
     outputStart
   );
+}
+
+export function useRef(
+  inputStart: MaybeRef<number>,
+  inputEnd: MaybeRef<number>,
+  outputStart: MaybeRef<number>,
+  outputEnd: MaybeRef<number>,
+  value: MaybeRef<number>
+) {
+  return computed(() => {
+    return (
+      ((unref(value) - unref(inputStart)) *
+        (unref(outputEnd) - unref(outputStart))) /
+        (unref(inputEnd) - unref(inputStart)) +
+      unref(outputStart)
+    );
+  });
 }
