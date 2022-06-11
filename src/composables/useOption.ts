2c8d2bd1bed515d3a1611e0ee63de62c4f38ba99
Zachary <zsakowitz@zsnout.com>
2022-06-11 08:42:23 -0700

    Add useOption

---
+++ src/composables/useOption.ts
@@ -0,0 +1,51 @@
+import { computed } from "@vue/reactivity";
+import { MaybeRef, syncRef, syncRefs, useUrlSearchParams } from "@vueuse/core";
+import {
+  toRef,
+  unref,
+  WritableComputedRef,
+  Ref,
+  watchEffect,
+  watch,
+} from "vue";
+import { useRoute } from "vue-router";
+
+const params = useUrlSearchParams("history");
+
+export interface Options {
+  string: string;
+  number: number;
+  boolean: boolean;
+}
+
+export function useOption(name: string, initial?: string) {
+  if (params[name] === undefined && initial !== undefined) {
+    params[name] = initial;
+  }
+
+  return computed<string>({
+    get() {
+      return "" + params[name];
+    },
+    set(value) {
+      params[name] = value;
+    },
+  });
+}
+
+export function useNumericOption(name: string, initial?: number) {
+  const option = useOption(name, "" + initial);
+
+  return computed<number>({
+    get() {
+      return parseFloat(option.value);
+    },
+    set(value) {
+      option.value = "" + value;
+    },
+  });
+}
+
+export function syncNumericOption(name: string, ref: Ref<number>) {
+  syncRef(useNumericOption(name, ref.value), ref);
+}
