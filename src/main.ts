43d8712b849dc7e50489916d7d35e2c8504b416c
Zachary <zsakowitz@zsnout.com>
2022-06-11 07:58:19 -0700

    Add compile-time trim tagged template literal macro

--- src/main.ts
+++ src/main.ts
@@ -54,5 +54,3 @@ watchEffect(() => (cssVar.value = height.value + "px"));
 if (typeof Object.hasOwn !== "function") {
   Object.hasOwn = Object.prototype.hasOwnProperty.call;
 }
-
-globalThis.trim = String.raw;
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,28 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 
+const jsfile = /\.([jt]sx?|vue)($|\?)/;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA()],
+  plugins: [
+    vue(),
+    {
+      name: "remove-trim",
+      transform(code, id) {
+        if (process.env.NODE_ENV === "development")
+          return code.replace(/trim`/g, "`");
+
+        if (jsfile.test(id)) {
+          return code.replace(/trim\s*(`[^`]*`)/g, (match) => {
+            return match
+              .slice(4)
+              .replace(/\s+/g, " ")
+              .replace(/\B \b|\b \B|\B \B/g, "");
+          });
+        }
+      },
+    },
+    VitePWA(),
+  ],
 });
