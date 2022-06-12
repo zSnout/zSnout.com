f847c5a4de70033e012082d4833781cedd2bbb14
Zachary <zsakowitz@zsnout.com>
2022-06-12 09:09:10 -0700

    Add dropdown

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+  import { ref, watch } from "vue";
+
+  const { modelValue } = defineProps<{
+    modelValue: string;
+  }>();
+
+  const emit = defineEmits<{
+    (event: "update:modelValue", modelValue: string): void;
+  }>();
+
+  const value = ref(modelValue);
+  watch(value, (val) => emit("update:modelValue", val));
+</script>
+
+<template>
+  <select class="field focusline second-layer">
+    <slot />
+  </select>
+</template>
+
+<style scoped>
+  .field {
+    display: block;
+    width: 100%;
+    font-size: inherit;
+  }
+</style>
