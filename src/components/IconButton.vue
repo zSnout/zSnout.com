8ec59015d2f6ca9062e79ddfc3b79a4d22ff9605
Zachary <zsakowitz@zsnout.com>
2022-06-12 08:01:24 -0700

    Add icons and IconButton

---
+++ src/components/IconButton.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+  import Button from "./Button.vue";
+</script>
+
+<template>
+  <Button center style="width: 2.5em; height: 2.25em">
+    <slot />
+  </Button>
+</template>
