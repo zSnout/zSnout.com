3d90efbef4a5eac82ad62b31289b74921693bd1f
Zachary <zsakowitz@zsnout.com>
2022-06-12 18:31:44 -0700

    Add InlineField and InlineNumericField

---
+++ src/components/InlineField.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+  import { toRef, watch } from "vue";
+
+  const props = defineProps<{
+    modelValue: string;
+    type?: string;
+    autocomplete?: string;
+    placeholder?: string;
+  }>();
+
+  defineEmits<{
+    (event: "update:modelValue", modelValue: string): void;
+  }>();
+
+  watch(toRef(props, "modelValue"), console.log);
+</script>
+
+<template>
+  <input
+    class="field"
+    :autocomplete="autocomplete ?? 'off'"
+    :placeholder="placeholder"
+    :type="type ?? 'text'"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target!.value)"
+  />
+</template>
+
+<style lang="scss" scoped>
+  .field {
+    font-size: inherit;
+    background-color: transparent;
+    border: 0;
+    outline: none;
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      margin: 0;
+      -webkit-appearance: none;
+    }
+  }
+</style>
---
+++ src/components/InlineNumericField.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+  import InlineField from "./InlineField.vue";
+
+  defineProps<{
+    modelValue: number;
+    autocomplete?: string;
+    placeholder?: string;
+  }>();
+
+  defineEmits<{
+    (event: "update:modelValue", modelValue: number): void;
+  }>();
+</script>
+
+<template>
+  <InlineField
+    :autocomplete="autocomplete"
+    :model-value="'' + modelValue"
+    :placeholder="placeholder"
+    type="number"
+    @update:model-value="$emit('update:modelValue', +$event)"
+  />
+</template>
