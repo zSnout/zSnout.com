b2948c4ad2a92078a0489aff8f4b82b1b4e7c6c8
Zachary <zsakowitz@zsnout.com>
2022-06-15 08:14:55 -0700

    Add CheckboxField

---
+++ src/components/InlineCheckboxField.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+  defineProps<{
+    modelValue: boolean;
+    type?: string;
+    autocomplete?: string;
+  }>();
+
+  defineEmits<{
+    (event: "update:modelValue", modelValue: boolean): void;
+  }>();
+</script>
+
+<template>
+  <input
+    class="field"
+    :autocomplete="autocomplete ?? 'off'"
+    :checked="modelValue"
+    :type="type ?? 'checkbox'"
+    @change="$emit('update:modelValue', $event.target!.checked)"
+  />
+</template>
+
+<style lang="scss" scoped>
+  .field {
+    font-size: inherit;
+    background-color: transparent;
+    border: 0;
+    outline: none;
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      margin: 0;
+      -webkit-appearance: none;
+    }
+  }
+</style>
