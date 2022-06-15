c2cf3bfd5b4c138d5139a011dd904809d9d41d21
Zachary <zsakowitz@zsnout.com>
2022-06-14 20:54:41 -0700

    Use rangetouch

--- src/components/InlineRangeField.vue
+++ src/components/InlineRangeField.vue
@@ -1,4 +1,8 @@
 <script lang="ts" setup>
+  import { MaybeElement, unrefElement } from "@vueuse/core";
+  import RangeTouch from "rangetouch";
+  import { onMounted, onScopeDispose, ref } from "vue";
+
   defineProps<{
     modelValue: number;
     autocomplete?: string;
@@ -10,10 +14,23 @@
   defineEmits<{
     (event: "update:modelValue", modelValue: number): void;
   }>();
+
+  let destroy: (() => void) | undefined;
+  onScopeDispose(() => destroy?.());
+
+  const field = ref<MaybeElement>();
+  onMounted(() => {
+    const el = unrefElement(field);
+    if (!el) return;
+
+    const range = new RangeTouch(el);
+    destroy = () => range.destroy;
+  });
 </script>
 
 <template>
   <input
+    ref="field"
     class="field"
     :autocomplete="autocomplete ?? 'off'"
     :max="max"
