471b970b053cdc14cf8e95437956a408b5313439
Zachary <zsakowitz@zsnout.com>
2022-06-15 07:21:15 -0700

    Change track size

--- src/components/InlineRangeField.vue
+++ src/components/InlineRangeField.vue
@@ -45,10 +45,10 @@
 <style lang="scss" scoped>
   .field {
     width: 100%;
-    height: 0.75em;
+    height: 0.5em;
     font-size: inherit;
     background: #d0d0d0;
-    border-radius: 0.375em;
+    border-radius: 0.25em;
     outline: none;
     opacity: 0.7;
     transition: var(--transitions), opacity 0.2s;
