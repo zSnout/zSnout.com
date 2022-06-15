9d83fe8468bdd0670e230f1ddd906c5f6309679b
Zachary <zsakowitz@zsnout.com>
2022-06-14 20:54:35 -0700

    Add rangetouch

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,12 @@
 /// <reference types="vite-plugin-pwa/client" />
 
 declare function trim(source: TemplateStringsArray): string;
+
+declare module "rangetouch" {
+  class RangeTouch {
+    constructor(element: Element);
+    destroy(): void;
+  }
+
+  export default RangeTouch;
+}
