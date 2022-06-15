297f77349f4475466130996401299bbd1539f324
Zachary <zsakowitz@zsnout.com>
2022-06-14 18:03:09 -0700

    Add InlineRangeField

---
+++ src/components/InlineRangeField.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+  defineProps<{
+    modelValue: number;
+    autocomplete?: string;
+    min?: number;
+    max?: number;
+    step?: number | "any";
+  }>();
+
+  defineEmits<{
+    (event: "update:modelValue", modelValue: number): void;
+  }>();
+</script>
+
+<template>
+  <input
+    class="field"
+    :autocomplete="autocomplete ?? 'off'"
+    :value="modelValue"
+    type="range"
+    @input="$emit('update:modelValue', +$event.target!.value)"
+  />
+</template>
+
+<style lang="scss" scoped>
+  .field {
+    width: 100%;
+    font-size: inherit;
+    background-color: transparent;
+    border: 0;
+    outline: none;
+
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      margin: 0;
+      -webkit-appearance: none;
+    }
+  }
+</style>
