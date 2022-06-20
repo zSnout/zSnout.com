8f6a4a5ec0eeb1f3b6dcdb1630f79a5e2a46d2a4
Zachary <zsakowitz@zsnout.com>
2022-06-20 08:46:36 -0700

    Add useResolved

---
+++ src/composables/useResolved.ts
@@ -0,0 +1,13 @@
+import { reactive } from "vue";
+
+export function useResolved<T>(promises: Iterable<T | PromiseLike<T>>) {
+  const result: T[] = reactive([]);
+
+  for (let item of promises) {
+    if ("then" in item && typeof item.then === "function") {
+      item.then((e) => result.push(e));
+    } else result.push(item as any);
+  }
+
+  return result;
+}
