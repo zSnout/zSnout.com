0961d399be8f33842a2e7546e0d7f3b2d263d7c7
Zachary <zsakowitz@zsnout.com>
2022-06-20 09:26:54 -0700

    Remove unused watchers

--- src/composables/usePageTitle.ts
+++ src/composables/usePageTitle.ts
@@ -1,11 +1,11 @@
-import { MaybeRef, useTitle } from "@vueuse/core";
+import { MaybeRef, tryOnScopeDispose, useTitle } from "@vueuse/core";
 import { isRef, watchEffect } from "vue";
 
 const docTitle = useTitle();
 
 export function usePageTitle(title: MaybeRef<string>) {
   if (isRef(title)) {
-    watchEffect(() => (docTitle.value = title.value));
+    tryOnScopeDispose(watchEffect(() => (docTitle.value = title.value)));
   } else {
     docTitle.value = `${title} | zSnout`;
   }
