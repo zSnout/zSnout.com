0dad386fc563f1412437d93d6b9416c4fdc78a67
Zachary <zsakowitz@zsnout.com>
2022-06-21 10:11:46 -0700

    Add MaybeLabeled

---
+++ src/components/MaybeLabeled.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+  import Labeled from "./Labeled.vue";
+
+  defineProps<{ label?: string }>();
+</script>
+
+<template>
+  <Labeled v-if="label" :label="label">
+    <slot />
+  </Labeled>
+
+  <slot v-else />
+</template>
