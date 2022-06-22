77cfb977efb8de87129dc5520ac28a76d00c2e7d
Zachary <zsakowitz@zsnout.com>
2022-06-21 18:29:35 -0700

    Add Console

---
+++ src/components/Console.vue
@@ -0,0 +1,259 @@
+<script lang="ts">
+  export interface Console extends globalThis.Console {
+    select(
+      label: string | undefined,
+      options: Record<string, string>
+    ): Promise<string>;
+
+    field(placeholder: string | undefined, required: true): Promise<string>;
+    field(
+      placeholder?: string,
+      required?: boolean
+    ): Promise<string | undefined>;
+
+    user(message?: any, ...optionalParams: any[]): void;
+  }
+
+  export interface CompleteConsole {
+    console: Console;
+    messages: Message[];
+    field: Ref<string>;
+    onSelect(id: number, item: string): void;
+    onSubmit(field: string | undefined): void;
+    placeholder: Ref<string | undefined>;
+  }
+
+  export function useCompleteConsole(): CompleteConsole {
+    const console: Console = {
+      ...globalThis.console,
+      error(message, ...optionalParams) {
+        messages.push({
+          type: "error",
+          id: Math.random(),
+          content: optionalParams.length
+            ? [message, ...optionalParams]
+            : message,
+        });
+      },
+      field(_placeholder, required): any {
+        placeholder.value = _placeholder;
+
+        return new Promise<string | undefined>((resolve) => {
+          if (required) {
+            onSubmit.push((value) => {
+              if (value) {
+                resolve(value);
+              } else return true;
+            });
+          } else onSubmit.push(resolve);
+        });
+      },
+      log(message, ...optionalParams) {
+        messages.push({
+          type: "log",
+          id: Math.random(),
+          content: optionalParams.length
+            ? [message, ...optionalParams]
+            : message,
+        });
+      },
+      select(label, options) {
+        const id = Math.random();
+
+        messages.push({
+          id,
+          label,
+          options,
+          type: "select",
+        });
+
+        return new Promise((resolve) => {
+          onSelect[id] = resolve;
+        });
+      },
+      user(message, ...optionalParams) {
+        messages.push({
+          type: "user",
+          id: Math.random(),
+          content: optionalParams.length
+            ? [message, ...optionalParams]
+            : message,
+        });
+      },
+      warn(message, ...optionalParams) {
+        messages.push({
+          type: "warn",
+          id: Math.random(),
+          content: optionalParams.length
+            ? [message, ...optionalParams]
+            : message,
+        });
+      },
+    };
+
+    const messages: Message[] = reactive([]);
+    const placeholder = ref<string | undefined>("");
+    const field = ref("");
+
+    const onSelect: Record<string, (value: string) => void> =
+      Object.create(null);
+
+    const onSubmit: ((value: string | undefined) => true | void)[] = [];
+
+    return {
+      console,
+      field,
+      messages,
+      placeholder,
+      onSelect(id, item) {
+        onSelect[id]?.(item);
+        delete onSelect[id];
+      },
+      onSubmit(field) {
+        onSubmit.splice(
+          0,
+          onSubmit.length,
+          ...onSubmit.filter((resolve) => resolve(field))
+        );
+      },
+    };
+  }
+</script>
+
+<script lang="ts" setup>
+  import { reactive, ref, Ref } from "vue";
+  import Button from "./Button.vue";
+  import Dropdown from "./Dropdown.vue";
+  import Field from "./Field.vue";
+  import HStack from "./HStack.vue";
+  import MaybeLabeled from "./MaybeLabeled.vue";
+  import VStack from "./VStack.vue";
+
+  export interface TextMessage {
+    type: "log" | "warn" | "error" | "user";
+    id: number;
+    content: any;
+  }
+
+  export interface SelectMessage {
+    type: "select";
+    id: number;
+    options: Record<string, string>;
+    selected?: string;
+    label?: string;
+  }
+
+  export type Message = TextMessage | SelectMessage;
+
+  const props = defineProps<{
+    messages: Message[];
+    field: string;
+    placeholder?: string;
+    stretch?: boolean;
+  }>();
+
+  const emit = defineEmits<{
+    (event: "update:messages", value: Message[]): void;
+    (event: "update:field", value: string): void;
+    (event: "select", id: number, item: string): void;
+    (event: "submit", field: string | undefined): void;
+  }>();
+
+  function submit() {
+    if (props.field) {
+      const message: Message = {
+        type: "user",
+        id: Math.random(),
+        content: props.field,
+      };
+      emit("update:messages", props.messages.concat(message));
+    }
+
+    emit("update:field", "");
+    emit("submit", props.field ? props.field : undefined);
+  }
+</script>
+
+<template>
+  <VStack :class="{ stretch }" class="console">
+    <template v-for="item in messages" :key="item.id">
+      <MaybeLabeled
+        v-if="item.type === 'select'"
+        class="log"
+        :label="item.label"
+        style="margin-bottom: 0.25em"
+      >
+        <Dropdown
+          class="select"
+          :disabled="!!item.selected"
+          :model-value="item.selected || ''"
+          @update:model-value="
+            $event &&
+              (item.selected = $event) &&
+              $emit('select', item.id, $event)
+          "
+        >
+          <option disabled value="">Choose an option...</option>
+
+          <option v-for="(option, key) in item.options" :key="key" :value="key">
+            {{ option }}
+          </option>
+        </Dropdown>
+      </MaybeLabeled>
+
+      <p v-else-if="Array.isArray(item.content)" class="log">
+        {{ item.content.join(" ") }}
+      </p>
+
+      <p v-else :class="item.type === 'log' ? '' : item.type" class="log">
+        {{ item.content }}
+      </p>
+    </template>
+
+    <form class="form" style="margin-top: auto" @submit.prevent="submit">
+      <HStack :space="0.75">
+        <Field
+          class="field"
+          :model-value="field || ''"
+          :placeholder="placeholder"
+          @update:model-value="$emit('update:field', $event)"
+        />
+
+        <Button @click="submit">✔</Button>
+      </HStack>
+    </form>
+  </VStack>
+</template>
+
+<style lang="scss" scoped>
+  .console {
+    overflow: auto;
+
+    &.stretch {
+      height: 100%;
+    }
+  }
+
+  .form {
+    position: sticky;
+    bottom: 0;
+  }
+
+  .log {
+    padding: 0 0.75rem;
+  }
+
+  label.log,
+  select.log {
+    padding-right: 0;
+  }
+
+  .field,
+  .log {
+    text-align: left;
+  }
+
+  .user {
+    font-style: italic;
+  }
+</style>
