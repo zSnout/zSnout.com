7179b719917bc9206268fbae9592994c4ab7711e
Zachary <zsakowitz@zsnout.com>
2022-06-23 11:44:05 -0700

    Add Node component

---
+++ src/components/Node.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+  import { onMounted, ref } from "vue";
+
+  const { node } = defineProps<{
+    node: Node;
+  }>();
+
+  const el = ref<HTMLElement>();
+
+  onMounted(() => {
+    el.value?.replaceWith(node);
+  });
+</script>
+
+<template>
+  <div ref="el" />
+</template>
