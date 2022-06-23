9f1cd47a5459780958ba8d86a056d64dfb5a858e
Zachary <zsakowitz@zsnout.com>
2022-06-23 16:12:27 -0700

    Sort symbols

--- src/composables/symbols.ts
+++ src/composables/symbols.ts
@@ -1,3 +1,3 @@
-export const PyodideSymbol = Symbol("pyodide");
 export const PyodideStdoutSymbol = Symbol("pyodide.stdout");
 export const PyodideStderrSymbol = Symbol("pyodide.stderr");
+export const PyodideSymbol = Symbol("pyodide");
