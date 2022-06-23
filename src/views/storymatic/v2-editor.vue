f743fa3a69d435eeb35f6fcb388bea3726d2f168
Zachary <zsakowitz@zsnout.com>
2022-06-23 15:04:43 -0700

    Add default value to v2 editor

--- src/views/storymatic/v2-editor.vue
+++ src/views/storymatic/v2-editor.vue
@@ -5,7 +5,40 @@
   import Viewer2 from "../../components/storymatic/Viewer2.vue";
   import { useLocationHash } from "../../composables/useLocationHash";
 
-  const code = useLocationHash();
+  const code = useLocationHash(`$money = 500
+$eggs = 0
+$name = (no name)
+
+@start
+  $name = @input What is your name?
+  @run market
+
+@market
+  $name, you have $$$money and $eggs dozen eggs.
+  @menu What would you like to buy?
+    Eggs
+      @eggs
+    Go Back
+      @pass
+  @market
+
+@eggs
+  $egg = @number How many dozen eggs do you want to buy?
+  $eggprice @= 6 * $egg
+  $egg dozen eggs will cost $$$eggprice.
+  @menu Are you sure you want to buy them?
+    @option Yes
+      @if $money > $eggprice
+        $money -= $eggprice
+        $eggs += $egg
+      @elseif $money = $eggprice
+        You have just enough money for these eggs.
+        $money -= $eggprice
+        $eggs += $egg
+      @else
+        You don't have enough money for these eggs!
+    @option No
+      Okay, let's go back to the store.`);
 </script>
 
 <template>
