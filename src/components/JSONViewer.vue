282d6b09351f1a9cb47a60b9fd98fae10afc86db
Zachary <zsakowitz@zsnout.com>
2022-06-23 14:55:57 -0700

    Add JSONViewer

---
+++ src/components/JSONViewer.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+  import ace from "ace-builds";
+  import AceEditor from "./AceEditor.vue";
+
+  defineProps<{
+    value?: any;
+    round?: boolean;
+  }>();
+
+  defineEmits<{
+    (event: "init", editor: ace.Ace.Editor): void;
+    (event: "save", value: string): void;
+  }>();
+</script>
+
+<template>
+  <AceEditor
+    :modelValue="JSON.stringify(value, undefined, '  ')"
+    :round="round"
+    mode="storymatic3"
+    readonly
+    @init="$emit('init', $event)"
+    @save="$emit('save', $event)"
+  />
+</template>
