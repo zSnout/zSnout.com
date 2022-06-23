2b391d15e309abba626d5195cf779b2817982cba
Zachary <zsakowitz@zsnout.com>
2022-06-23 10:16:50 -0700

    Change default v1 program

--- src/views/storymatic/v1.vue
+++ src/views/storymatic/v1.vue
@@ -5,7 +5,44 @@
   import Viewer1 from "../../components/storymatic/Viewer1.vue";
   import { useLocationHash } from "../../composables/useLocationHash";
 
-  const code = useLocationHash();
+  const code = useLocationHash(`@global $money = 500
+@global $eggs = 0
+@global $name = (no name)
+$eggprice = 6 <-- Try changing this value!
+
+start
+  @if $name = (no name)
+    $name = @input What is your name?
+  @run market
+
+market
+  You have $$money and $eggs dozen eggs.
+  @menu What would you like to buy?
+    Eggs
+      @context
+        @run eggs
+    Go Back
+      @pass
+  @run market
+
+eggs
+  $egg = @number How many dozen eggs do you want to buy?
+  $eggprice *= $egg
+  $egg dozen eggs will cost $$eggprice.
+  @menu Are you sure you want to buy them?
+    Yes
+      @if $money > $eggprice
+        $money -= $eggprice
+        $eggs += $egg
+      @elseif $money = $eggprice
+        You have just enough money for these eggs.
+        $money -= $eggprice
+        $eggs += $egg
+      @else
+        You don't have enough money for these eggs!
+    No
+      Okay, let's go back to the store.
+  @stop`);
 </script>
 
 <template>
