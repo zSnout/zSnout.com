8ff888bb66fa458cc5322cfdafb4b7d3227bb223
Zachary <zsakowitz@zsnout.com>
2022-06-23 18:29:44 -0700

    Add better placeholders

--- src/views/storymatic/v4-editor.vue
+++ src/views/storymatic/v4-editor.vue
@@ -4,7 +4,37 @@
   import Viewer4 from "../../components/storymatic/Viewer4.vue";
   import { useLocationHash } from "../../composables/useLocationHash";
 
-  const code = useLocationHash();
+  const code = useLocationHash(`money = 500
+eggs = 0
+
+name = prompt "What is your name?"
+await market()
+
+market = ->
+  print "#{name}, you have $#{money} and #{eggs} dozen eggs."
+  idx = await select "What would you like to buy?", ["Eggs", "Go Back"]
+  if idx == 0
+    await eggs()
+  await market()
+
+eggs = ->
+  egg = +(prompt "How many dozen eggs do you want to buy?")
+  eggprice = 6 * egg
+  print "#{egg} dozen eggs will cost $#{eggprice}."
+  if money > eggprice
+    idx = await select "Are you sure you want to buy them?", ["Yes", "No"]
+    if idx == 0
+      money -= eggprice
+      eggs += egg
+    else
+      print "Okay, let's go back to the store."
+  else if money == eggprice
+    print "You have just enough money for these eggs."
+    money -= eggprice
+    eggs += egg
+  else
+    print "You don't have enough money for these eggs!"
+`);
 </script>
 
 <template>
