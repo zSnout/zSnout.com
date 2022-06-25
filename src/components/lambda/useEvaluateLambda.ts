ea940022cd5bd72bb7139956f2919e2cf66f01f3
Zachary <zsakowitz@zsnout.com>
2022-06-25 08:52:00 -0700

    Improve reserved word support

--- src/components/lambda/useEvaluateLambda.ts
+++ src/components/lambda/useEvaluateLambda.ts
@@ -31,7 +31,11 @@ function normalizeVar(name: string) {
     .map((e) => (e in special ? (special as any)[e] : e))
     .join("");
 
-  if (name.match(/^\d/)) {
+  if (
+    name.match(
+      /^\d|^(abstract|arguments|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield)$/
+    )
+  ) {
     return `_${name}`;
   }
 
