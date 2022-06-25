a352fdfbba14522ed271511a77bfc939b322d201
Zachary <zsakowitz@zsnout.com>
2022-06-25 07:23:37 -0700

    Add initial preset

--- src/views/lambdaCalculus/Compiler.vue
+++ src/views/lambdaCalculus/Compiler.vue
@@ -3,29 +3,9 @@
   import Compiler from "../../components/lambda/Compiler.vue";
   import Editor from "../../components/lambda/Editor.vue";
   import { useLocationHash } from "../../composables/useLocationHash";
+  import initial from "../../components/lambda/preset.txt?raw";
 
-  const code = useLocationHash(
-    `
-U = λf.(f f)
-Y = λf.(λx.(x x) λx.(f λy.((x x) y)))
-void = λx.(U U)
-
-succ = λn.λf.λx.f(n f x)
-0 = λf.λx.x
-1 = λf.λx.f x
-2 = succ 1
-3 = succ 2
-4 = succ 3
-5 = succ 4
-6 = succ 5
-7 = succ 6
-8 = succ 7
-9 = succ 8
-10 = succ 9
-
-10
-`.trim()
-  );
+  const code = useLocationHash(initial);
 </script>
 
 <template>
--- src/views/lambdaCalculus/Evaluator.vue
+++ src/views/lambdaCalculus/Evaluator.vue
@@ -2,30 +2,10 @@
   import EditorAndViewer from "../../components/EditorAndViewer.vue";
   import Editor from "../../components/lambda/Editor.vue";
   import Evaluator from "../../components/lambda/Evaluator.vue";
+  import initial from "../../components/lambda/preset.txt?raw";
   import { useLocationHash } from "../../composables/useLocationHash";
 
-  const code = useLocationHash(
-    `
-U = λf.(f f)
-Y = λf.(λx.(x x) λx.(f λy.((x x) y)))
-void = λx.(U U)
-
-succ = λn.λf.λx.f(n f x)
-0 = λf.λx.x
-1 = λf.λx.f x
-2 = succ 1
-3 = succ 2
-4 = succ 3
-5 = succ 4
-6 = succ 5
-7 = succ 6
-8 = succ 7
-9 = succ 8
-10 = succ 9
-
-10
-`.trim()
-  );
+  const code = useLocationHash(initial);
 </script>
 
 <template>
