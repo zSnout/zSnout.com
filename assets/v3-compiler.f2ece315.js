d338a60b4dfae0bcd498c7e231b239bb6b319030
zsakowitz <zsakowitz@users.noreply.github.com>
2022-06-26 17:13:09 +0000

    deploy: 2a4f5d8492bacd9ed83031fd5a2e228b2445cc9f

---
+++ assets/v3-compiler.f2ece315.js
@@ -0,0 +1,31 @@
+import{u as i,E as d}from"./useLocationHash.d6ae8332.js";import{A as l}from"./AceEditor.c5dd813d.js";import"./mode-javascript.e6176182.js";import{s as c}from"./compiler3.8ff8648d.js";import{d as u,ac as p,o as g,c as m,u as t,w as $,b as s,x as y}from"./index.b934a723.js";import{_ as f}from"./Editor3.3cca401f.js";import"./DocumentDisplay.fa87fee7.js";import"./Modal.2fcbc3dd.js";const _=u({__name:"Compiler3",props:{code:null,round:{type:Boolean}},setup(o){const e=o,r=p(()=>c(e.code));return(n,a)=>(g(),m(l,{"model-value":t(r),round:o.round,mode:"javascript",readonly:""},null,8,["model-value","round"]))}}),B=u({__name:"v3-compiler",setup(o){const e=i(`$money = 500
+$eggs = 0
+
+$name = @input "What is your name?"
+@market
+
+def @market
+  "$name, you have $$money and $eggs dozen eggs."
+  @menu "What would you like to buy?"
+    @option "Eggs"
+      @eggsfn
+    @option "Go Back"
+  @market
+
+def @eggsfn
+  $egg = @inputint "How many dozen eggs do you want to buy?"
+  $eggprice = 6 * $egg
+  "$egg dozen eggs will cost $$eggprice."
+  @menu "Are you sure you want to buy them?"
+    @option "Yes"
+      if $money > $eggprice
+        $money -= $eggprice
+        $eggs += $egg
+      else if $money = $eggprice
+        "You have just enough money for these eggs."
+        $money -= $eggprice
+        $eggs += $egg
+      else
+        "You don't have enough money for these eggs!"
+    @option "No"
+      "Okay, let's go back to the store."`);return(r,n)=>(g(),m(d,{"dual-editor":""},{default:$(()=>[s(f,{modelValue:t(e),"onUpdate:modelValue":n[0]||(n[0]=a=>y(e)?e.value=a:null),round:""},null,8,["modelValue"]),s(_,{code:t(e),round:""},null,8,["code"])]),_:1}))}});export{B as default};
