51ef4efb45bacd9785ad0c770ebd205adbca1be8
Zachary <zsakowitz@zsnout.com>
2022-06-27 10:12:35 -0700

    Add LargeTitle

---
+++ src/components/LargeTitle.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  import Title from "./Title.vue";
+</script>
+
+<template>
+  <Title class="title">
+    <slot />
+  </Title>
+</template>
+
+<style lang="scss" scoped>
+  .title {
+    font-size: min(3em, 10vw);
+  }
+</style>
