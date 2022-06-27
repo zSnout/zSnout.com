10e3254dea09b6b77b716eb0e582aa1e000ab25f
Zachary <zsakowitz@zsnout.com>
2022-06-27 16:20:20 -0700

    Add MaybeLink

---
+++ src/components/MaybeLink.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+  import Link from "./Link.vue";
+
+  defineProps<{
+    to?: string;
+    inline?: boolean;
+  }>();
+</script>
+
+<template>
+  <Link v-if="to !== undefined" :to="to">
+    <slot />
+  </Link>
+
+  <span v-else-if="inline">
+    <slot />
+  </span>
+
+  <div v-else>
+    <slot />
+  </div>
+</template>
