3650d4b6f80bb5474cd327e9810a64e8f69c0ad4
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-06-27 06:06:08 +0000

    Add art/Index

---
+++ src/views/art/Index.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+  import Card from "../../components/Card.vue";
+  import DocumentDisplay from "../../components/DocumentDisplay.vue";
+  import SearchableCardGrid from "../../components/SearchableCardGrid.vue";
+  import Title from "../../components/Title.vue";
+</script>
+
+<template>
+  <DocumentDisplay>
+    <Title>Art Pages</Title>
+
+    <SearchableCardGrid>
+      <Card
+        description="Create your own blobby gradient with a custom size and color scheme."
+        keywords="circles"
+        title="Blobby Gradient"
+        to="/art/blobby-gradient"
+      />
+
+      <Card
+        description="Watch an amazing display of colorful rectangles and change their sizes."
+        keywords="squares"
+        title="Rectangles"
+        to="/art/rectangles"
+      />
+
+      <Card
+        description="View a vast and vivid variety of vibrant ellipses."
+        keywords="circles"
+        title="Ellipses"
+        to="/art/ellipses"
+      />
+    </SearchableCardGrid>
+  </DocumentDisplay>
+</template>
