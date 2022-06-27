d086a107cda891eee95267e7892964d1bb37541c
Zachary <zsakowitz@zsnout.com>
2022-06-26 17:49:50 -0700

    Add randomized lines

--- src/views/Spinner.vue
+++ src/views/Spinner.vue
@@ -21,11 +21,10 @@
 
   useCanvas(canvas, { useDevicePixelRatio: false }).then((info) => {
     const { canvas, onDispose, onResize, size } = info;
+    const { width, height } = size;
     const ctx = canvas.getContext("2d")!;
 
-    const maxDist = computed(() =>
-      Math.min(size.width.value / 2, size.height.value / 2)
-    );
+    const maxDist = computed(() => Math.min(width.value / 2, height.value / 2));
 
     if (!ctx) {
       throw new Error("Your device doesn't support HTML5 canvases.");
@@ -34,18 +33,12 @@
     let lastX = NaN;
     let lastY = NaN;
 
-    ctx.translate(size.width.value / 2, size.height.value / 2);
+    ctx.translate(width.value / 2, height.value / 2);
 
     function drawCircle() {
       ctx.beginPath();
       ctx.fillStyle = "white";
-      ctx.arc(
-        size.width.value / 2,
-        size.height.value / 2,
-        maxDist.value,
-        0,
-        2 * Math.PI
-      );
+      ctx.arc(width.value / 2, height.value / 2, maxDist.value, 0, 2 * Math.PI);
       ctx.fill();
     }
 
@@ -61,15 +54,9 @@
           return;
         }
 
-        const dist = Math.hypot(
-          size.width.value / 2 - x,
-          size.height.value / 2 - y
-        );
+        const dist = Math.hypot(width.value / 2 - x, height.value / 2 - y);
 
-        const angle = Math.atan2(
-          y - size.height.value / 2,
-          x - size.width.value / 2
-        );
+        const angle = Math.atan2(y - height.value / 2, x - width.value / 2);
 
         if (dist > maxDist.value) {
           lastX = NaN;
@@ -78,8 +65,8 @@
         }
 
         [x, y] = [
-          size.width.value / 2 + dist * Math.cos(rotation.value - angle),
-          size.height.value / 2 - dist * Math.sin(rotation.value - angle),
+          width.value / 2 + dist * Math.cos(rotation.value - angle),
+          height.value / 2 - dist * Math.sin(rotation.value - angle),
         ];
 
         if (!isNaN(lastX) && !isNaN(lastY)) {
@@ -93,6 +80,22 @@
         lastY = y;
       })
     );
+
+    setTimeout(() => {
+      const rand = () => Math.random() * 200 - 100;
+
+      for (let [i, j] of [
+        [0, 0],
+        [-200, 100],
+        [100, 300],
+        [70, -250],
+      ]) {
+        ctx.beginPath();
+        ctx.moveTo(width.value / 2 + i + rand(), height.value / 2 + j + rand());
+        ctx.lineTo(width.value / 2 + i + rand(), height.value / 2 + j + rand());
+        ctx.stroke();
+      }
+    });
   });
 
   useEventListener("pointerdown", () => (isDown.value = true));
