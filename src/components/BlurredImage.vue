72af20acfdeed2375259dc4a50b1f8c8a13c74d9
Zachary <zsakowitz@zsnout.com>
2022-06-28 10:50:13 -0700

    Add parallax effect

--- src/components/BlurredImage.vue
+++ src/components/BlurredImage.vue
@@ -1,14 +1,20 @@
 <script lang="ts" setup>
   import { computed } from "@vue/reactivity";
+  import { usePointer } from "@vueuse/core";
   import { ref } from "vue";
-  import { appWidth } from "../main";
+  import { appHeight, appWidth } from "../main";
 
-  defineProps<{ src: string }>();
+  defineProps<{
+    sensitivity?: number;
+    src: string;
+  }>();
 
   const visible = ref(false);
 
   const x = Math.random();
   const y = Math.random();
+
+  const { x: px, y: py } = usePointer();
 
   const padding = computed(() => {
     if (appWidth.value < 400) {
@@ -33,8 +39,12 @@
     :src="src"
     :style="{
       display: visible ? undefined : 'none',
-      top: `calc(${padding} + (var(--app-height) - ${padding} - 2em - 320px) * ${y})`,
-      left: `calc(${lrPadding} + (var(--app-width) - 2 * ${lrPadding} - 320px) * ${x})`,
+      top: `calc(${padding} + (var(--app-height) - ${padding} - 2em - 320px) * ${y} + ${
+        py - appHeight / 2
+      }px / 100 * ${sensitivity ?? 1})`,
+      left: `calc(${lrPadding} + (var(--app-width) - 2 * ${lrPadding} - 320px) * ${x} + ${
+        px - appWidth / 2
+      }px / 100 * ${sensitivity ?? 1})`,
     }"
     @load="visible = true"
   />
