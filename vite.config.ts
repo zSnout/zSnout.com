c67e4b540fbae91f0a9f9d97bc7978bc9d621b8a
Zachary <zsakowitz@zsnout.com>
2022-06-28 11:18:46 -0700

    Create virtual:image-list

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
+import { sync } from "fast-glob";
 
 const jsfile = /\.([jt]sx?|vue)($|\?)/;
+const images = sync("./public/images/**/*.png");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,6 +31,21 @@ export default defineConfig({
       transform(code, id) {
         if (jsfile.test(id)) {
           return code.replace(/_Storymatic/g, "Storymatic");
+        }
+      },
+    },
+    {
+      name: "image-list",
+      resolveId(id) {
+        if (id === "virtual:image-list") {
+          return "\0virtual:image-list";
+        }
+      },
+      load(id) {
+        if (id === "\0virtual:image-list") {
+          return `export default ['${images
+            .map((e) => e.slice(8))
+            .join("', '")}']`;
         }
       },
     },
