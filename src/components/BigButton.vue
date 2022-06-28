b46aa923fa5fcc6cf83a608703dc1b0bf20331e9
Zachary <zsakowitz@zsnout.com>
2022-06-28 13:51:19 -0700

    Externalize BigButton

---
+++ src/components/BigButton.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+  import Button from "./Button.vue";
+</script>
+
+<template>
+  <Button class="button">
+    <slot />
+  </Button>
+</template>
+
+<style scoped>
+  .button {
+    width: min(max(50%, 500px), 100%);
+    height: min(max(33%, 300px), 100%);
+    font-size: 2em;
+  }
+</style>
