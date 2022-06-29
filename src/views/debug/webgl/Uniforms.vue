6b3af3cce754b051782ea274cc624f782f0f6013
Zachary <zsakowitz@zsnout.com>
2022-06-29 12:59:39 -0700

    Upgrade debug/uniforms to the new system

--- src/views/debug/webgl/Uniforms.vue
+++ src/views/debug/webgl/Uniforms.vue
@@ -1,52 +1,48 @@
 <script lang="ts" setup>
-  import { MaybeElement, useInterval } from "@vueuse/core";
-  import { computed, ref } from "vue";
+  import { useInterval } from "@vueuse/core";
+  import { computed, onMounted, ref } from "vue";
   import FullscreenDisplay from "../../../components/FullscreenDisplay.vue";
-  import { useWebGL } from "../../../composables/useWebGL";
+  import { WebGlCanvas } from "../../../composables/webgl/WebGlCanvas";
 
-  const canvas = ref<MaybeElement>();
-  useWebGL(
-    canvas,
-    trim`
-    in vec2 pos;
-    out vec4 color;
+  const canvas = ref<HTMLCanvasElement>();
 
-    uniform float left;
+  const shader = trim`
+  uniform vec2 u_resolution;
+  uniform float left;
 
-    void main() {
-      if (pos.x < left) {
-        color = vec4(1, 1, 0, 1);
-      } else if (pos.y < 0.01) {
-        color = vec4(1, 0, 0, 1);
-      } else if (pos.x > 0.99) {
-        color = vec4(0, 1, 0, 1);
-      } else if (pos.y > 0.99) {
-        color = vec4(0, 0, 1, 1);
-      } else {
-        color = vec4(pos.x, pos.y, 0.5, 1);
-      }
-    }`
-  ).then((gl) => {
-    const count = useInterval(10, { immediate: true });
+  varying vec4 v_position;
+
+  void main() {
+    vec2 pos = gl_FragCoord.xy / u_resolution;
+
+    if (pos.x < left) {
+      gl_FragColor = vec4(1, 1, 0, 1);
+    } else if (pos.y < 0.01) {
+      gl_FragColor = vec4(1, 0, 0, 1);
+    } else if (pos.x > 0.99) {
+      gl_FragColor = vec4(0, 1, 0, 1);
+    } else if (pos.y > 0.99) {
+      gl_FragColor = vec4(0, 0, 1, 1);
+    } else {
+      gl_FragColor = vec4(pos.xy, 0.5, 1);
+    }
+  }`;
+
+  onMounted(() => {
+    if (!canvas.value) return;
+
+    const gl = new WebGlCanvas(canvas.value, {
+      fragmentString: shader,
+    });
+
+    const count = useInterval(10);
     const left = computed(() => (count.value / 1000) % 1);
-
-    gl.useUniform("left", "f", left);
+    gl.useUniform("left", left);
   });
 </script>
 
 <template>
   <FullscreenDisplay>
     <canvas ref="canvas" />
-
-    <template #options>
-      <p><strong>What is this?</strong></p>
-
-      <p>
-        This is a WebGL debug page. The left side should be a yellow bar, the
-        right a green bar, the top blue, and the bottom red. A gradient should
-        be contained within these with a bottom-left corner of blue, a top-left
-        of mint, a top-right of yellow, and a bottom-right of magenta.
-      </p>
-    </template>
   </FullscreenDisplay>
 </template>
