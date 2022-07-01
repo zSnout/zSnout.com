279e6271174c06ac45ba0835fdc76df085774b9c
Zachary <zsakowitz@zsnout.com>
2022-07-01 09:45:25 -0700

    Improve database.ts

--- server/database.ts
+++ server/database.ts
@@ -1,5 +1,27 @@
 import { MongoClient } from "mongodb";
 
 export const client = process.env.ZSNOUT_DATABASE
-  ? new MongoClient(process.env.ZSNOUT_DATABASE).connect()
+  ? new MongoClient(process.env.ZSNOUT_DATABASE)
+      .connect()
+      .then(async (client) => {
+        const db = client.db("zsnout");
+
+        return {
+          collection<T extends keyof Database>(name: T) {
+            return db.collection<Database[T]>(name);
+          },
+        };
+      })
   : undefined;
+
+export interface Database {
+  accounts: {
+    id: string;
+    creation: number;
+    username: string;
+    password: string;
+    session: string;
+    email: string;
+    verified: boolean;
+  };
+}
