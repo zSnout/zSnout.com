9cf83526d287adf4294a030a8c62207ac7aa41bf
Zachary <zsakowitz@zsnout.com>
2022-07-01 12:57:45 -0700

    Fix issues

--- server/database.ts
+++ server/database.ts
@@ -4,21 +4,21 @@ const client = process.env.ZSNOUT_DATABASE
   ? new MongoClient(process.env.ZSNOUT_DATABASE, { serverApi: "1" })
       .connect()
       .catch((e) => (console.log(e), undefined))
-  : (console.log("no database available"), undefined);
+  : Promise.resolve((console.log("no database available"), undefined));
 
 const db = client
-  ?.then((client) => client?.db("zsnout"))
+  .then((client) => client?.db("zsnout"))
   .catch((e) => (console.log(e), undefined));
 
 export const isActive =
-  client?.then((e) => (e ? true : false)) ?? Promise.resolve(false);
+  client.then((e) => (e ? true : false)) ?? Promise.resolve(false);
 
 export async function collection<T extends keyof Database>(name: T) {
   return (await db)?.collection<Database[T]>(name);
 }
 
 process.on("beforeExit", () => {
-  client?.then((client) => client?.close());
+  client.then((client) => client?.close());
 });
 
 export interface Database {
