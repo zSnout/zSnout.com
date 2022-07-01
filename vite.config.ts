a3615e0f49c52e350fc740cad169e39e60e4658c
Zachary <zsakowitz@zsnout.com>
2022-07-01 08:54:23 -0700

    Add server.ts as handler

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { VitePWA } from "vite-plugin-pwa";
 import { sync } from "fast-glob";
+import mix from "vite-plugin-mix";
 
 const jsfile = /\.([jt]sx?|vue)($|\?)/;
 const images = sync("./public/images/**/*.png");
@@ -34,5 +35,6 @@ export default defineConfig({
       },
     },
     VitePWA(),
+    mix({ handler: "./server.ts" }),
   ],
 });
