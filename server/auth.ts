9a79ff747a32dc7378eaa24c88343bb0e6af15f4
Zachary <zsakowitz@zsnout.com>
2022-07-01 13:54:36 -0700

    Improve auth.ts

--- server/auth.ts
+++ server/auth.ts
@@ -1,22 +1,32 @@
 import { compare, hash } from "bcrypt";
-import { ObjectId } from "bson";
+import { randomUUID } from "crypto";
 import { collection } from "./database";
+import { send } from "./email";
 
-export function isLegalUsername(username: string) {
-  return (
-    username.length >= 5 &&
-    /^[A-Za-z_][A-Za-z0-9_]+$/.test(username) &&
-    !/zsnout/i.test(username)
-  );
-}
+export namespace IsLegal {
+  export function Username(username: string) {
+    return (
+      username.length >= 5 &&
+      username.length <= 20 &&
+      /^[A-Za-z_][A-Za-z0-9_]+$/.test(username) &&
+      !/zsnout/i.test(username)
+    );
+  }
 
-export function isLegalPassword(password: string) {
-  return (
-    password.length >= 8 &&
-    /(?![\d_])\w/.test(password) &&
-    /\d/.test(password) &&
-    /[^\s\w]/.test(password)
-  );
+  export function Password(password: string) {
+    return (
+      password.length >= 8 &&
+      /(?![\d_])\w/.test(password) &&
+      /\d/.test(password) &&
+      /[^\s\w]/.test(password)
+    );
+  }
+
+  export function Email(email: string) {
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+      email
+    );
+  }
 }
 
 export function hashPassword(password: string) {
@@ -29,7 +39,7 @@ export function verifyPassword(password: string, hashed: string) {
 
 const _accounts = collection("accounts");
 
-export async function authenticateUser(username: string, password: string) {
+export async function logInUser(username: string, password: string) {
   const accounts = await _accounts;
   if (!accounts) return { status: AuthStatus.NoServer as const };
 
@@ -43,7 +53,14 @@ export async function authenticateUser(username: string, password: string) {
   }
 }
 
-export async function reauthenticateUser(session: string) {
+export enum AuthStatus {
+  BadPassword,
+  NoServer,
+  NoUser,
+  Success,
+}
+
+export async function checkCredentials(session: string) {
   const accounts = await _accounts;
   if (!accounts) return { status: ReAuthStatus.NoServer as const };
 
@@ -53,15 +70,68 @@ export async function reauthenticateUser(session: string) {
   return { status: ReAuthStatus.Success as const, account };
 }
 
-export enum AuthStatus {
-  BadPassword,
+export enum ReAuthStatus {
   NoServer,
-  NoUser,
+  Failure,
   Success,
 }
 
-export enum ReAuthStatus {
+export async function createAccount(
+  username: string,
+  password: string,
+  email: string
+) {
+  const accounts = await _accounts;
+  if (!accounts) return { status: AccountStatus.NoServer as const };
+
+  if (!IsLegal.Username(username))
+    return { status: AccountStatus.BadUsername as const };
+
+  if (!IsLegal.Password(password))
+    return { status: AccountStatus.BadPassword as const };
+
+  if (!IsLegal.Email(email)) return { status: AccountStatus.BadEmail as const };
+
+  if (await accounts.findOne({ username }))
+    return { status: AccountStatus.UsernameTaken as const };
+
+  if (await accounts.findOne({ email }))
+    return { status: AccountStatus.EmailTaken as const };
+
+  const account = {
+    username,
+    password,
+    email,
+    creation: Date.now(),
+    session: randomUUID(),
+    verified: false,
+    verifyCode: randomUUID(),
+  };
+
+  const result = await accounts.insertOne(account);
+  if (!result.acknowledged) return { status: AccountStatus.Failure as const };
+
+  const sentMessage = await send({
+    to: email,
+    subject: "Verify your zSnout account",
+    text: `Hey @${username}! Verify your new zSnout account by opening this link: https://zsnout.com/account/verify?code=${account.verifyCode}.`,
+  });
+
+  if (!sentMessage) {
+    await accounts.deleteOne({ session: account.session });
+    return { status: AccountStatus.BadEmail as const };
+  }
+
+  return { status: AccountStatus.Success as const, account };
+}
+
+export enum AccountStatus {
   NoServer,
+  BadUsername,
+  BadPassword,
+  BadEmail,
+  UsernameTaken,
+  EmailTaken,
   Failure,
   Success,
 }
