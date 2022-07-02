8db61360d4923b6c98c541df529642cfdfa4f298
zsakowitz <zsakowitz@users.noreply.github.com>
2022-07-02 12:10:50 +0000

    deploy: 2699ad064b005613e14175c20fb9181660eee43a

---
+++ assets/Title.8f010710.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./Modal.dc2c4486.js";import{o,f as r,n as a}from"./index.70e8d914.js";const d={},c={class:"title"};function n(e,_){return o(),r("h1",c,[a(e.$slots,"default",{},void 0,!0)])}var i=t(d,[["render",n],["__scopeId","data-v-00c7d846"]]);export{i as T};
