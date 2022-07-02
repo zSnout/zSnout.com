c3d426ec332379e7fde96c1f05379e738124bc0e
zsakowitz <zsakowitz@users.noreply.github.com>
2022-07-02 13:41:40 +0000

    deploy: 817bd369eac1304c8d5313d09d501fb52828416b

---
+++ assets/Grid.eb1a0fae.js
@@ -0,0 +1,11 @@
+import{F as s}from"./FullscreenDisplay.765026f4.js";import{C as n}from"./CoordinateCanvas2d.1c1d3c8b.js";import{d as r,k as i,B as l,c as p,w as t,o as c,a as o}from"./index.b085a846.js";import"./Modal.74b639bc.js";import"./WebGlCanvas.52909bf2.js";import"./useOption.58894b67.js";const f=o("p",null,[o("strong",null,"What is this?")],-1),d=o("p",null," This is a WebGL debug page. A gradient should be contained within these with a bottom-left corner of blue, a top-left of mint, a top-right of yellow, and a bottom-right of magenta. There should also be a grid of black lines. ",-1),x=r({__name:"Grid",setup(u){const a=i(),e=`
+  void main() {
+    gl_FragColor = vec4(pos.x, pos.y, 0.5, 1);
+
+    if (
+      abs(float(int(pos.x)) - pos.x) < 0.01 ||
+      abs(float(int(pos.y)) - pos.y) < 0.01
+    ) {
+      gl_FragColor = vec4(0, 0, 0, 1);
+    }
+  }`;return l(()=>{!a.value||new n(a.value,{fragmentString:e})}),(m,h)=>(c(),p(s,null,{options:t(()=>[f,d]),default:t(()=>[o("canvas",{ref_key:"canvas",ref:a},null,512)]),_:1}))}});export{x as default};
