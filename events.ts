c531b8268edbf479d5876de6b2de604da1024b29
Zachary <zsakowitz@zsnout.com>
2022-07-02 16:01:33 -0700

    Add more events

--- events.ts
+++ events.ts
@@ -1,8 +1,16 @@
 export interface ClientToServer {
   "account:check-session"(session: string): void;
+
+  "account:create-account"(
+    username: string,
+    password: string,
+    email: string
+  ): void;
 }
 
 export interface ServerToClient {
   "account:update:session"(session: string): void;
   "account:update:username"(username: string): void;
+  "go-home"(): void;
+  "set-error"(error: string): void;
 }
