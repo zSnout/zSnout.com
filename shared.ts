6cd02057d24302536cd9d9250f9acb719a25c555
Zachary <zsakowitz@zsnout.com>
2022-07-02 22:36:57 -0700

    Add more shared things

--- shared.ts
+++ shared.ts
@@ -1,19 +1,21 @@
 export interface ClientToServer {
   "account:check-session"(session: string): void;
   "account:create"(username: string, password: string, email: string): void;
+  "account:login"(username: string, password: string): void;
+  "account:verify"(verifyCode: string): void;
 
-  "bookmarks:request"(): void;
-  "bookmarks:update"(bookmarks: Bookmark[]): void;
+  "bookmarks:request"(session: string): void;
+  "bookmarks:update"(session: string, bookmarks: Bookmark[]): void;
 }
 
 export interface ServerToClient {
   "account:update:session"(session: string): void;
   "account:update:username"(username: string): void;
+  "account:complete-login"(): void;
 
   "bookmarks:list"(bookmarks: Bookmark[]): void;
 
   "error"(error: string): void;
-  "go-home"(): void;
 }
 
 export interface Bookmark {
