47a272971b4d3bc4f98188987be9e3f864004156
zsakowitz <zsakowitz@users.noreply.github.com>
2022-07-26 01:30:06 +0000

    deploy: 78622de35bafd5f155beb4e4bf3f5059fe0bdabb

---
+++ assets/Mandelbrot.54a80104.js
@@ -0,0 +1,14 @@
+import{F as s}from"./FullscreenDisplay.2191a18a.js";import{M as n}from"./MovableCanvas2d.d53b547d.js";import{d as r,k as i,B as l,c,w as o,o as f,a as e}from"./index.ea2cbfa9.js";import"./BookmarkIcon.5e433a48.js";import"./CoordinateCanvas2d.75073387.js";import"./WebGlCanvas.29ba3931.js";import"./useOption.1ca588e9.js";const p=e("p",null,[e("strong",null,"What is this?")],-1),d=e("p",null," This is a WebGL debug page. It should contain a simple rendition of the Mandelbrot Set. This is used for testing purposes and helps demonstrate different features of the MovableCanvas mixin. ",-1),C=r({__name:"Mandelbrot",setup(u){const a=i(),t=`
+  void main() {
+    gl_FragColor = vec4(0, 0, 0, 1);
+
+    vec2 z;
+    for (int i = 0; i < 100; i++) {
+      z = vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + pos;
+
+      if (z.x * z.x + z.y * z.y > 4.0) {
+        gl_FragColor = vec4(float(i) / 100.0, float(i) / 100.0, float(i) / 100.0, 1.0);
+        break;
+      }
+    }
+  }`;return l(()=>{!a.value||new n(a.value,{fragmentString:t})}),(m,_)=>(f(),c(s,null,{options:o(()=>[p,d]),default:o(()=>[e("canvas",{ref_key:"canvas",ref:a},null,512)]),_:1}))}});export{C as default};
--- assets/Title.8c54593b.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./BookmarkIcon.4168f441.js";import{o,f as r,n as a}from"./index.b7459f53.js";const d={},c={class:"title"};function n(e,_){return o(),r("h1",c,[a(e.$slots,"default",{},void 0,!0)])}var i=t(d,[["render",n],["__scopeId","data-v-00c7d846"]]);export{i as T};
