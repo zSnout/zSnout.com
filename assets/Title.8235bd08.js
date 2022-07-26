e680fdd7b8498723d7a7d3859ebae78da926ef3d
zsakowitz <zsakowitz@users.noreply.github.com>
2022-07-26 18:44:14 +0000

    deploy: 7bfab3fccc6511c26d79b15e7e35788d267ed3d1

--- assets/Title.8235bd08.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./BookmarkIcon.54a7509f.js";import{o,f as r,n as a}from"./index.80c17931.js";const d={},c={class:"title"};function n(e,_){return o(),r("h1",c,[a(e.$slots,"default",{},void 0,!0)])}var i=t(d,[["render",n],["__scopeId","data-v-00c7d846"]]);export{i as T};
