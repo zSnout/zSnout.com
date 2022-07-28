c63bbeedb24c140694e971f6d487cc5910d1a08d
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-07-28 13:48:27 +0000

    Add notes-related events

--- shared.server.d.ts
+++ shared.server.d.ts
@@ -1,3 +1,5 @@
+import { ObjectIdLike } from "bson";
+
 export interface ClientToServer {
   "account:check-session"(session: string): void;
   "account:create"(username: string, password: string, email: string): void;
@@ -6,6 +8,16 @@ export interface ClientToServer {
 
   "bookmarks:request"(session: string): void;
   "bookmarks:update"(session: string, bookmarks: Bookmark[]): void;
+
+  "notes:create"(session: string, title: string): void;
+  "notes:request:details"(session: string, noteId: string): void;
+  "notes:request:index"(session: string): void;
+  "notes:request:note"(session: string, noteId: string): void;
+  "notes:update:details"(
+    session: string,
+    noteId: string,
+    details: NoteDetails
+  ): void;
 }
 
 export interface ServerToClient {
@@ -17,6 +29,10 @@ export interface ServerToClient {
   "bookmarks:list"(bookmarks: Bookmark[]): void;
 
   "error"(error: string): void;
+
+  "notes:index"(notes: NotePreview[]): void;
+  "notes:note"(note: string): void;
+  "notes:details"(details: NoteDetails): void;
 }
 
 export interface Bookmark {
@@ -28,4 +44,22 @@ export interface ChatMessage {
   creation: number;
   sentBy: string; // username of sender at the send time
   message: string;
+}
+
+export interface NoteDetails {
+  id: string;
+  contributors:
+    | {
+        username: string;
+        level: "owner" | "editor" | "viewer";
+      }[]
+    | false;
+  baseRole: "editor" | "viewer";
+  title: string;
+}
+
+export interface NotePreview {
+  title: string;
+  desc: string;
+  id: string;
 }
