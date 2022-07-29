f4c0e78379cce0135b98d848b94faffce7ba511c
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-07-29 13:01:25 +0000

    Add Note.vue

---
+++ src/views/Note.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+  import Collaboration from "@tiptap/extension-collaboration";
+  import StarterKit from "@tiptap/starter-kit";
+  import { Editor } from "@tiptap/vue-3";
+  import { onUnmounted } from "vue";
+  import { useRoute } from "vue-router";
+  import * as Y from "yjs";
+  import DocumentDisplay from "../components/DocumentDisplay.vue";
+
+  const { id } = useRoute().params;
+
+  const doc = new Y.Doc();
+
+  const editor = new Editor({
+    extensions: [
+      StarterKit.configure({ history: false }),
+      Collaboration.configure({ document: doc }),
+    ],
+  });
+
+  onUnmounted(() => editor.destroy());
+</script>
+
+<template>
+  <DocumentDisplay>
+    <p>Sorry, editing notes hasn't been added yet.</p>
+  </DocumentDisplay>
+</template>
