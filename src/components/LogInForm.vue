03617f9082c006b58ec9dc3d4179d6720dfaf257
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-08-02 19:10:00 -0400

    Update LogInForm.vue

--- src/components/LogInForm.vue
+++ src/components/LogInForm.vue
@@ -17,7 +17,7 @@
   const isSigningUp = ref(signUp || false);
   error.value = "";
 
-  socket.on("account:done:login", () => emit("update:open", false));
+  socket.on("account:done:verify", () => emit("update:open", false));
   socket.on("error", () => (disabled.value = false));
 
   function logIn() {
