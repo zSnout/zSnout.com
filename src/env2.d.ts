947ea25874fbc1f0638499d0245e687996fab0a8
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-08-05 14:37:07 +0000

    Declare markdown files as components

--- src/env2.d.ts
+++ src/env2.d.ts
@@ -1,4 +1,11 @@
+import { DefineComponent } from "vue";
+
 declare module "virtual:image-list" {
   const images: readonly string[];
   export default images;
+}
+
+declare module "*.md" {
+  const comp: DefineComponent;
+  export default comp;
 }
