ca5bbd6b249f9f4a51dacb8f53d0840607d8aaeb
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-08-06 12:24:17 +0000

    Add MiddleText

---
+++ src/components/MiddleText.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="middle">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  .middle {
+    width: 500px;
+    max-width: min(500px, 100%);
+    text-align: left;
+  }
+</style>
