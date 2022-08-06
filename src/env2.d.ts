724d9cfc2b2d4043f28f4db099be8b8aefa2db4d
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-08-06 14:19:16 +0000

    fix things

--- src/env2.d.ts
+++ src/env2.d.ts
@@ -1,11 +1,11 @@
-import { DefineComponent } from "vue";
-
 declare module "virtual:image-list" {
   const images: readonly string[];
   export default images;
 }
 
 declare module "*.md" {
+  import { DefineComponent } from "vue";
+
   const comp: DefineComponent;
   export default comp;
 }
