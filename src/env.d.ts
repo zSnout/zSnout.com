13ea660b6f3ec239f9f326b600bdf1fd8c808a6d
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-08-06 23:57:51 +0000

    Add generate-maze

--- src/env.d.ts
+++ src/env.d.ts
@@ -8,6 +8,38 @@ import {
   PyodideSymbol,
 } from "./composables/symbols";
 
+declare module "@vueuse/core" {
+  export function useEventListener<T extends keyof HTMLElementEventMap>(
+    target: HTMLElement,
+    event: T,
+    handler: (evt: HTMLElementEventMap[T]) => void
+  );
+}
+
+declare module "ace-builds" {
+  export function define(
+    module: string,
+    factory: (require: (dep: string) => any, exports: any) => void
+  ): void;
+
+  export function define(
+    module: string,
+    deps: string[],
+    factory: (require: (dep: string) => any, exports: any) => void
+  ): void;
+}
+
+declare module "generate-maze" {
+  export interface MazeCell {
+    x: number;
+    y: number;
+    top: boolean;
+    left: boolean;
+    bottom: boolean;
+    right: boolean;
+  }
+}
+
 declare module "rangetouch" {
   export interface RangeTouchOptions {
     addCSS?: boolean;
@@ -29,27 +61,6 @@ declare module "rangetouch" {
   }
 
   export default RangeTouch;
-}
-
-declare module "ace-builds" {
-  export function define(
-    module: string,
-    factory: (require: (dep: string) => any, exports: any) => void
-  ): void;
-
-  export function define(
-    module: string,
-    deps: string[],
-    factory: (require: (dep: string) => any, exports: any) => void
-  ): void;
-}
-
-declare module "@vueuse/core" {
-  export function useEventListener<T extends keyof HTMLElementEventMap>(
-    target: HTMLElement,
-    event: T,
-    handler: (evt: HTMLElementEventMap[T]) => void
-  );
 }
 
 declare global {
