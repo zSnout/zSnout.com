edebd60565496324d5773c71b5ca36006a4f9205
zsakowitz <zsakowitz@users.noreply.github.com>
2022-08-07 01:03:42 +0000

    deploy: 0ba566f404f6f896877b504df72d4a81b2b5d5a6

---
+++ assets/Title.caedda16.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./BookmarkIcon.701950e0.js";import{o,f as r,n as a}from"./index.a72ff2fb.js";const d={},c={class:"title"};function n(e,_){return o(),r("h1",c,[a(e.$slots,"default",{},void 0,!0)])}var i=t(d,[["render",n],["__scopeId","data-v-00c7d846"]]);export{i as T};
