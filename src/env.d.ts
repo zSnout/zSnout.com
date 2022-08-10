2e8b7a64e9e2ffb5e141337b17c435039bb35abb
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-08-10 02:20:36 +0000

    Declare setPointerCapture on $event.target

--- src/env.d.ts
+++ src/env.d.ts
@@ -69,6 +69,9 @@ declare global {
     [PyodideStdoutSymbol](msg: string): void;
     [PyodideSymbol]: ReturnType<typeof loadPyodide>;
   }
+
+  declare interface EventTarget
+    extends Pick<HTMLElement, "setPointerCapture"> {}
 }
 
 export {};
