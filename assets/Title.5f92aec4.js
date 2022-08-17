80edc3665730af84ced33cce677aecff5a07929d
zsakowitz <zsakowitz@users.noreply.github.com>
2022-08-17 19:45:23 +0000

    deploy: 064a959519e9677a9428721ee9016cfbc4911db3

--- assets/Title.5f92aec4.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./VStack.a77958dd.js";import{o,f as r,m as a}from"./index.4027b45f.js";const d={},c={class:"title"};function _(e,n){return o(),r("h1",c,[a(e.$slots,"default",{},void 0,!0)])}var i=t(d,[["render",_],["__scopeId","data-v-00c7d846"]]);export{i as T};
