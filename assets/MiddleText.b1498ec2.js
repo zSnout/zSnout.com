a5937880a3c64b271f1124deac1106f8df4b7151
zsakowitz <zsakowitz@users.noreply.github.com>
2022-08-18 01:22:44 +0000

    deploy: 8550c9d6da91d933799fc4f29e6d9306c15b4238

---
+++ assets/MiddleText.b1498ec2.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./VStack.a8ad7d87.js";import{o as d,f as _,m as a}from"./index.775470d7.js";const r={},n={class:"middle"};function o(e,s){return d(),_("div",n,[a(e.$slots,"default",{},void 0,!0)])}var l=t(r,[["render",o],["__scopeId","data-v-70bc5044"]]);export{l as M};
