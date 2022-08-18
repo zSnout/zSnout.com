c73839176c053a1980d606658bb579090652b32c
zsakowitz <zsakowitz@users.noreply.github.com>
2022-08-18 01:35:27 +0000

    deploy: 45a5f0576ebf52255c708b8f357ddda0463c09b0

---
+++ assets/CameraModifier.53d30f02.js
@@ -0,0 +1,13 @@
+import{u as i,_}from"./ColorSliders.d13be449.js";import{F as v}from"./FullscreenDisplay.9a388f22.js";import{u as g}from"./useCanvas.5f788445.js";import{u as C,a as x}from"./useVideoFromStream.575b0822.js";import{W as h}from"./WebGlCanvas.16e66b54.js";import{d as w,s as u,o as F,c as S,w as o,b,u as k,a as l}from"./index.c653be01.js";import"./useOption.cf3f5a1d.js";import"./InlineRangeField.2bdb80c8.js";import"./VStack.eb3d5a98.js";import"./Labeled.b5094b26.js";import"./SharedNav.cbddc32d.js";const U=w({__name:"CameraModifier",setup(y){const s=u(),a=i(),e=u("user"),{stream:c,setOpts:m}=C({audio:!1,video:{facingMode:{ideal:e.value}}});function d(){e.value=e.value==="user"?"environment":"user",m({audio:!1,video:{facingMode:{ideal:e.value}}})}const f=x(c,!0),p=`
+  uniform sampler2D u_texture;
+  uniform vec2 u_resolution;
+
+  ${i}
+
+  void main() {
+    vec3 color = texture2D(u_texture, gl_FragCoord.xy / u_resolution).rgb;
+    color = use_color_sliders(color.rbg);
+
+    gl_FragColor = vec4(color, 1.0);
+  }
+  `;return g(s).then(t=>{const{canvas:n}=t,r=new h(n,{fragmentString:p});r.setTexture("u_texture",f),r.on("render",()=>a.setGlsl(r))}),(t,n)=>(F(),S(v,null,{options:o(()=>[b(_,{sliders:k(a)},null,8,["sliders"])]),indicator:o(()=>[l("span",{style:{cursor:"pointer"},onClick:d},"Switch Camera")]),default:o(()=>[l("canvas",{ref_key:"canvas",ref:s},null,512)]),_:1}))}});export{U as default};
