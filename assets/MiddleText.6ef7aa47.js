69ff28574cca318290323060608216c90730880d
zsakowitz <zsakowitz@users.noreply.github.com>
2022-08-20 17:43:27 +0000

    deploy: 781ccba036c7e5e3377da90417e2ffc86cd01f16

---
+++ assets/MiddleText.6ef7aa47.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./VStack.d9a6c01a.js";import{o as d,f as _,q as a}from"./index.3457413b.js";const r={},n={class:"middle"};function o(e,s){return d(),_("div",n,[a(e.$slots,"default",{},void 0,!0)])}var l=t(r,[["render",o],["__scopeId","data-v-70bc5044"]]);export{l as M};
---
+++ assets/v3-compiler.6cc5f984.js
@@ -0,0 +1,31 @@
+import{u as i,E as d}from"./useLocationHash.ff844aeb.js";import{A as l}from"./AceEditor.6ad21c32.js";import"./mode-javascript.e6176182.js";import{s as p}from"./compiler3.e1bd1ac4.js";import{d as u,ah as c,o as g,c as m,u as t,w as $,b as s,aR as y}from"./index.3457413b.js";import{_ as f}from"./Editor3.e38ba2df.js";import"./DocumentDisplay.b5231834.js";import"./SharedNav.720d082d.js";import"./VStack.d9a6c01a.js";const h=u({__name:"Compiler3",props:{code:null,round:{type:Boolean}},setup(o){const e=o,r=c(()=>p(e.code));return(n,a)=>(g(),m(l,{"model-value":t(r),round:o.round,mode:"javascript",readonly:""},null,8,["model-value","round"]))}}),E=u({__name:"v3-compiler",setup(o){const e=i(`$money = 500
+$eggs = 0
+
+$name = @input "What is your name?"
+@market
+
+def @market
+  "$name, you have $$money and $eggs dozen eggs."
+  @menu "What would you like to buy?"
+    @option "Eggs"
+      @eggsfn
+    @option "Go Back"
+  @market
+
+def @eggsfn
+  $egg = @inputint "How many dozen eggs do you want to buy?"
+  $eggprice = 6 * $egg
+  "$egg dozen eggs will cost $$eggprice."
+  @menu "Are you sure you want to buy them?"
+    @option "Yes"
+      if $money > $eggprice
+        $money -= $eggprice
+        $eggs += $egg
+      else if $money = $eggprice
+        "You have just enough money for these eggs."
+        $money -= $eggprice
+        $eggs += $egg
+      else
+        "You don't have enough money for these eggs!"
+    @option "No"
+      "Okay, let's go back to the store."`);return(r,n)=>(g(),m(d,{"dual-editor":""},{default:$(()=>[s(f,{modelValue:t(e),"onUpdate:modelValue":n[0]||(n[0]=a=>y(e)?e.value=a:null),round:""},null,8,["modelValue"]),s(h,{code:t(e),round:""},null,8,["code"])]),_:1}))}});export{E as default};
