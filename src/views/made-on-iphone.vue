7e504302a6ef83d3b9fbde78f8e252053cbd799c
Zachary Sakowitz <zsakowitz@zsnout.com>
2022-08-20 23:01:40 +0000

    Add made-on-iphone

---
+++ src/views/made-on-iphone.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+  import DocumentDisplay from "../components/DocumentDisplay.vue";
+  import Title from "../components/Title.vue";
+  import MiddleText from "../components/MiddleText.vue";
+</script>
+
+<template>
+  <DocumentDisplay>
+    <MiddleText>
+      <Title>Made on iPhone</Title>
+
+      <p>
+        I literally made this whole page on an iPhone using a bluetooth
+        keyboard. No clue who added this as a feature along with keyboard
+        shortcuts but it's cool.
+      </p>
+    </MiddleText>
+  </DocumentDisplay>
+</template>
