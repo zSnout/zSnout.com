ccc939ba8c92828dd198f55cafce05393da41721
Zachary <zsakowitz@zsnout.com>
2022-08-22 09:45:19 -0700

    Add useShuffle

---
+++ src/composables/useShuffle.ts
@@ -0,0 +1,19 @@
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+
+export function shuffle<T>(array: readonly T[]): readonly T[] {
+  const _array = [...array];
+  let currentIndex = _array.length;
+  let randomIndex: number;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [_array[currentIndex], _array[randomIndex]] = [
+      _array[randomIndex],
+      _array[currentIndex],
+    ];
+  }
+
+  return _array;
+}
