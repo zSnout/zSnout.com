63be273a5371bfe64dbdf93f3b65786e19df40b0
Zachary <zsakowitz@zsnout.com>
2022-08-22 08:13:37 -0700

    Add center and maxWidth to PageContent

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -1,9 +1,35 @@
 <script lang="ts" setup>
   import SafeArea from "./SafeArea.vue";
+  import Spacer from "./Spacer.vue";
+
+  defineProps<{
+    center?: boolean;
+    maxWidth?: boolean;
+  }>();
 </script>
 
 <template>
-  <SafeArea bottom style="flex: 1" top>
+  <template v-if="center">
+    <div class="centered">
+      <Spacer />
+
+      <SafeArea :max-width="maxWidth" bottom top>
+        <slot />
+      </SafeArea>
+
+      <Spacer />
+    </div>
+  </template>
+
+  <SafeArea v-else :max-width="maxWidth" bottom style="flex: 1" top>
     <slot />
   </SafeArea>
 </template>
+
+<style scoped>
+  .centered {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+  }
+</style>
