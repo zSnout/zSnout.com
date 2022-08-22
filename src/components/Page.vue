85913d71f3dcb0510fe1fb0bdbbff4ef6115f17b
Zachary <zsakowitz@zsnout.com>
2022-08-22 07:39:27 -0700

    Add Page

---
+++ src/components/Page.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="page">
+    <slot />
+  </section>
+</template>
+
+<style lang="scss" scoped>
+  .page {
+    display: flex;
+    flex-direction: column;
+    width: var(--app-width);
+    min-height: calc(var(--app-height) - 3.5rem);
+    overflow: hidden;
+    scroll-snap-align: start;
+
+    @media (max-width: 400px) {
+      min-height: calc(var(--app-height) - 2.5rem);
+    }
+
+    > :deep(:first-child) {
+      margin-top: 0;
+    }
+
+    > :deep(:last-child) {
+      margin-bottom: 0;
+    }
+  }
+</style>
