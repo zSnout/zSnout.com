822f4d317edcabd7e995c2266b92aa6ce9d714a8
Zachary <zsakowitz@zsnout.com>
2022-08-22 09:32:57 -0700

    Update Spacer

--- src/components/Spacer.vue
+++ src/components/Spacer.vue
@@ -3,11 +3,14 @@
 </script>
 
 <template>
-  <div v-for="() in +(size ?? 1)" class="spacer" />
+  <div v-for="() in +(size ?? 1)" class="spacer">
+    <slot>i</slot>
+  </div>
 </template>
 
 <style scoped>
   .spacer {
     flex: 1;
+    color: transparent;
   }
 </style>
