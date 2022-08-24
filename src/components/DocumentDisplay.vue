3f875e023bce5d5c4fe4a7aed9ee1119b1412f11
Zachary <zsakowitz@zsnout.com>
2022-08-23 18:53:55 -0700

    Use Footer and Aside

--- src/components/DocumentDisplay.vue
+++ src/components/DocumentDisplay.vue
@@ -1,13 +1,10 @@
 <script lang="ts" setup>
-  import { UseScreenSafeArea } from "@vueuse/components";
   import { useMediaQuery } from "@vueuse/core";
   import { computed } from "vue";
-  import { session } from "../main";
-  import HStack from "./HStack.vue";
-  import Logo from "./Logo.vue";
+  import Aside from "./Aside.vue";
+  import Footer from "./Footer.vue";
   import Navigation from "./Navigation.vue";
   import SafeArea from "./SafeArea.vue";
-  import VStack from "./VStack.vue";
 
   defineProps<{
     center?: boolean;
@@ -40,6 +37,7 @@
 
   <SafeArea
     class="page-content"
+    :aside="!!$slots.aside"
     :explicit-height="explicitHeight"
     :flex="center"
     :max-width="maxWidth"
@@ -50,50 +48,11 @@
     <slot />
   </SafeArea>
 
-  <footer class="blur footer second-layer">
-    <UseScreenSafeArea bottom>
-      <SafeArea bottom top>
-        <VStack :space="1">
-          <p>
-            <Logo invert style="height: 2em" />
-          </p>
+  <Aside v-if="$slots.aside">
+    <slot name="aside" />
+  </Aside>
 
-          <HStack no-center stretch wraps="400px">
-            <VStack>
-              <p>&copy; {{ new Date().getFullYear() }} Zachary Sakowitz</p>
-
-              <RouterLink class="no-color" to="/privacy-policy">
-                Privacy Policy
-              </RouterLink>
-
-              <p>
-                zSnout promises never to share any of your personal information
-                with third parties.
-              </p>
-            </VStack>
-
-            <VStack>
-              <RouterLink class="no-color" to="/">Homepage</RouterLink>
-
-              <RouterLink v-if="session" class="no-color" to="/my-account">
-                My Account
-              </RouterLink>
-
-              <p>
-                All content of the zSnout website is copyrighted under the
-                <a
-                  href="https://www.gnu.org/licenses/gpl-3.0.en.html"
-                  target="_blank"
-                >
-                  GNU General Public License.
-                </a>
-              </p>
-            </VStack>
-          </HStack>
-        </VStack>
-      </SafeArea>
-    </UseScreenSafeArea>
-  </footer>
+  <Footer :aside="!!$slots.aside" />
 </template>
 
 <style lang="scss">
@@ -106,11 +65,5 @@
 <style lang="scss" scoped>
   .page-content {
     z-index: 2;
-  }
-
-  .footer {
-    z-index: 3;
-    border-radius: 0;
-    box-shadow: none !important;
   }
 </style>
--- src/components/MultiPageDisplay.vue
+++ src/components/MultiPageDisplay.vue
@@ -1,13 +1,8 @@
 <script lang="ts" setup>
-  import { UseScreenSafeArea } from "@vueuse/components";
   import { useCssVar } from "@vueuse/core";
-  import { onUnmounted } from "vue";
-  import { session } from "../main";
-  import HStack from "./HStack.vue";
-  import Logo from "./Logo.vue";
+  import Aside from "./Aside.vue";
+  import Footer from "./Footer.vue";
   import Navigation from "./Navigation.vue";
-  import SafeArea from "./SafeArea.vue";
-  import VStack from "./VStack.vue";
 
   const props = defineProps<{ snap?: boolean }>();
   const snap = useCssVar("--snap");
@@ -37,50 +32,11 @@
     <slot />
   </div>
 
-  <footer class="blur footer second-layer">
-    <UseScreenSafeArea bottom>
-      <SafeArea bottom top>
-        <VStack :space="1">
-          <p>
-            <Logo invert style="height: 2em" />
-          </p>
+  <Aside v-if="$slots.aside">
+    <slot name="aside" />
+  </Aside>
 
-          <HStack no-center stretch wraps="400px">
-            <VStack>
-              <p>&copy; {{ new Date().getFullYear() }} Zachary Sakowitz</p>
-
-              <RouterLink class="no-color" to="/privacy-policy">
-                Privacy Policy
-              </RouterLink>
-
-              <p>
-                zSnout promises never to share any of your personal information
-                with third parties.
-              </p>
-            </VStack>
-
-            <VStack>
-              <RouterLink class="no-color" to="/">Homepage</RouterLink>
-
-              <RouterLink v-if="session" class="no-color" to="/my-account">
-                My Account
-              </RouterLink>
-
-              <p>
-                All content of the zSnout website is copyrighted under the
-                <a
-                  href="https://www.gnu.org/licenses/gpl-3.0.en.html"
-                  target="_blank"
-                >
-                  GNU General Public License.
-                </a>
-              </p>
-            </VStack>
-          </HStack>
-        </VStack>
-      </SafeArea>
-    </UseScreenSafeArea>
-  </footer>
+  <Footer class="footer" :aside="!!$slots.aside" />
 </template>
 
 <style lang="scss">
@@ -106,9 +62,6 @@
   }
 
   .footer {
-    z-index: 3;
-    border-radius: 0;
-    box-shadow: none !important;
     scroll-snap-align: start;
   }
 </style>
