698c2eebf9288c30898d1a7d3cbfc66da361f966
Zachary <zsakowitz@zsnout.com>
2022-08-23 18:53:59 -0700

    Add debug/asides

---
+++ src/views/debug/asides.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  import DocumentDisplay from "../../components/DocumentDisplay.vue";
+</script>
+
+<template>
+  <DocumentDisplay>
+    <template #aside>
+      <h1>hello world</h1>
+
+      <p>my aside</p>
+    </template>
+
+    <h1>main content</h1>
+  </DocumentDisplay>
+</template>
