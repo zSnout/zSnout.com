b796c08f9b574cbee0f39b04cc63200274cd31a6
Zachary <zsakowitz@zsnout.com>
2022-08-25 17:44:08 -0700

    Add scroll-margin-top to Prose elements

--- src/components/Prose.vue
+++ src/components/Prose.vue
@@ -81,6 +81,11 @@
     h3 {
       color: #25575b;
       font-weight: 900;
+      scroll-margin-top: 3.5rem + 2rem;
+
+      @media (max-width: 400px) {
+        scroll-margin-top: 2.5rem + 1.5rem;
+      }
 
       .dark & {
         color: #a3d5d6;
