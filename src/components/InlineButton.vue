7cb196a1e216d8503d51fc96d0a7c803c7de4995
Zachary <zsakowitz@zsnout.com>
2022-08-27 08:26:05 -0700

    Add InlineButton

---
+++ src/components/InlineButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button class="link">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+  .link {
+    padding: 0;
+    font-size: inherit;
+    border: none;
+    appearance: none;
+  }
+</style>
