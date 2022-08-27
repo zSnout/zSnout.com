09cad48a8216a2a660754e04f520b296358dc98b
Zachary <zsakowitz@zsnout.com>
2022-08-27 08:48:18 -0700

    Add filters

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,15 +1,17 @@
 <script lang="ts" setup>
+  import { useMediaQuery } from "@vueuse/core";
   import images from "virtual:image-list";
   import { ref } from "vue";
   import BlurredImage from "../components/BlurredImage.vue";
   import Cover from "../components/Cover.vue";
   import DocumentDisplay from "../components/DocumentDisplay.vue";
   import ImageCard from "../components/ImageCard.vue";
+  import InlineButton from "../components/InlineButton.vue";
   import LargeTitle from "../components/LargeTitle.vue";
   import LogInForm from "../components/LogInForm.vue";
   import SearchableCardGrid from "../components/SearchableCardGrid.vue";
   import VStack from "../components/VStack.vue";
-  import { homeIcons } from "../composables/useHomeIcons";
+  import { Group, groups, homeIcons } from "../composables/useHomeIcons";
   import { useRandomItem } from "../composables/useRandomItem";
   import { timeLeftBeforeAccountDeletion, username } from "../main";
 
@@ -18,6 +20,8 @@
   const c = useRandomItem(images);
 
   const open = ref(false);
+  const filter = ref<Group | "">("");
+  const isDesktop = useMediaQuery("(min-width: 1080px)");
 </script>
 
 <template>
@@ -30,57 +34,74 @@
   <LogInForm v-model:open="open" />
 
   <DocumentDisplay>
-    <Cover>
-      <VStack class="stack" :space="2">
-        <LargeTitle v-if="username">
-          Welcome to zSnout, {{ username }}.
-        </LargeTitle>
+    <VStack class="stack" :space="2">
+      <LargeTitle v-if="username">
+        Welcome to zSnout, {{ username }}.
+      </LargeTitle>
 
-        <LargeTitle v-else>Welcome to zSnout.</LargeTitle>
+      <LargeTitle v-else>Welcome to zSnout.</LargeTitle>
 
-        <p class="text-color" style="line-height: 2">
-          zSnout is a website made by
-          <!-- prettier-ignore -->
-          <a href="https://github.com/zsakowitz" target="_blank" class="link">Zachary Sakowitz</a>
-          to create and test various projects. This edition, zSnout 7, is a
-          collection of many of these projects and their archived versions, such
-          as the four versions of Storymatic. You can support Zachary at
-          <!-- prettier-ignore -->
-          <a class="link" href="https://store.zsnout.com/" target="_blank">the zSnout store.</a>
-        </p>
-
-        <p class="text-color" style="margin-top: -1em">
-          Scroll down and select a page!
-        </p>
-
-        <p v-if="username && timeLeftBeforeAccountDeletion !== false">
-          Hey {{ username }}! Your account will be deleted in
-          {{
-            timeLeftBeforeAccountDeletion >= 120 * 1000
-              ? `${~~(timeLeftBeforeAccountDeletion / (60 * 1000))} minutes`
-              : `${~~(timeLeftBeforeAccountDeletion / 1000)} seconds`
-          }}
-          unless you verify it with the email we sent you.
-        </p>
-      </VStack>
-    </Cover>
+      <p v-if="username && timeLeftBeforeAccountDeletion !== false">
+        Hey {{ username }}! Your account will be deleted in
+        {{
+          timeLeftBeforeAccountDeletion >= 120 * 1000
+            ? `${~~(timeLeftBeforeAccountDeletion / (60 * 1000))} minutes`
+            : `${~~(timeLeftBeforeAccountDeletion / 1000)} seconds`
+        }}
+        unless you verify it with the email we sent you.
+      </p>
+    </VStack>
 
     <SearchableCardGrid :sizes="['normal', 'small', 'icon']">
-      <ImageCard
-        v-for="card in homeIcons"
-        :alt="card.alt"
-        :description="card.description"
-        :src="card.src"
-        :title="card.title"
-        :to="card.to === 'login' ? undefined : card.to"
-        @click="card.to === 'login' && (open = true)"
-      />
+      <template v-for="(card, index) in homeIcons" :key="index">
+        <ImageCard
+          v-if="!filter || card.group.includes(filter)"
+          :alt="card.alt"
+          :description="card.description"
+          :src="card.src"
+          :title="card.title"
+          :to="card.to === 'login' ? undefined : card.to"
+          @click="card.to === 'login' && (open = true)"
+        />
+      </template>
     </SearchableCardGrid>
 
     <template #help>
-      Looks like you've found the Help menu! Many pages on zSnout have this
-      button in the top-right corner. Simply click it to reveal instructions and
-      more details about the page you're viewing.
+      <p>
+        Looks like you've found the Help menu! Many pages on zSnout have this
+        button in the top-right corner. Simply click it to reveal instructions
+        and more details about the page you're viewing.
+      </p>
+
+      <p>
+        Here's a tip for this page:
+        {{
+          isDesktop
+            ? "use the sidebar to filter the pages."
+            : "use the table of contents to filter the pages."
+        }}
+      </p>
+    </template>
+
+    <template #aside>
+      <p><strong>Filter By:</strong></p>
+
+      <nav>
+        <ul>
+          <li>
+            <InlineButton @click="filter = ''">Reset Filter</InlineButton>
+          </li>
+
+          <li v-for="name in groups" :key="name">
+            <InlineButton
+              :active="filter === name"
+              @click="filter === name ? (filter = '') : (filter = name)"
+            >
+              {{ name[0].toUpperCase() + name.slice(1) }}
+            </InlineButton>
+          </li>
+        </ul>
+      </nav>
     </template>
   </DocumentDisplay>
 </template>
@@ -88,6 +109,7 @@
 <style scoped>
   .stack {
     position: relative;
+    margin-bottom: 2em;
     user-select: none;
     pointer-events: none;
   }
