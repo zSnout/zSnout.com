01bb12e700b20bf4408ab47e6cc3581a518c5b8d
Zachary <zsakowitz@zsnout.com>
2022-08-27 17:04:46 -0700

    Add corner saying "menu"

--- src/components/ImageCard.vue
+++ src/components/ImageCard.vue
@@ -7,6 +7,7 @@
     alt: string;
     description: string | Ref<string>;
     forceReload?: boolean;
+    isIndex?: boolean;
     keywords?: string;
     src: string;
     title: string | Ref<string>;
@@ -49,15 +50,57 @@
 
     <p class="title text-color">{{ title }}</p>
     <p class="description">{{ description }}</p>
+
+    <div
+      v-if="isIndex"
+      class="corner drop-shadow"
+      title="This page has a list of other subpages. Click to explore further."
+    >
+      <div class="corner-clip">
+        <div class="corner-text">menu</div>
+      </div>
+    </div>
   </MaybeLink>
 </template>
 
 <style lang="scss" scoped>
   .card {
+    position: relative;
     display: block;
     overflow: hidden;
     text-decoration: none;
     cursor: pointer;
+
+    .corner {
+      position: absolute;
+      top: -1px;
+      right: -1px;
+      display: block;
+      width: 4em;
+      height: 4em;
+    }
+
+    .corner-clip {
+      height: 100%;
+      background-color: #1f1f1f;
+      clip-path: polygon(100% 0%, 100% 100%, 0% 0%);
+
+      .dark & {
+        background-color: white;
+      }
+    }
+
+    .corner-text {
+      height: 50%;
+      color: white;
+      text-align: right;
+      transform-origin: 50% 50%;
+      rotate: 45deg;
+
+      .dark & {
+        color: black;
+      }
+    }
 
     .no-title.no-title & {
       padding-bottom: 0;
