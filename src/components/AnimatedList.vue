f999e752c6a140dad57108ea4c0ee2c15d7aa57e
Zachary <zsakowitz@zsnout.com>
2022-09-06 08:20:01 -0700

    Add AnimatedList

---
+++ src/components/AnimatedList.vue
@@ -0,0 +1,26 @@
+<template>
+  <TransitionGroup name="list">
+    <slot />
+  </TransitionGroup>
+</template>
+
+<style>
+  /* We can't use `scoped` here or the animations break due to Vue's nesting rules. */
+
+  .list-move,
+  .list-enter-active,
+  .list-leave-active {
+    transition: opacity 0.5s, transform 0.5s;
+  }
+
+  .list-enter-from,
+  .list-leave-to {
+    transform: translateX(30px);
+    opacity: 0;
+  }
+
+  .list-leave-active {
+    position: absolute;
+    width: inherit;
+  }
+</style>
