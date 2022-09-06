7dfe45bafc8798f1e946832f31178f38edba8205
Zachary <zsakowitz@zsnout.com>
2022-09-06 14:09:14 -0700

    Make bingo tiles merge together

--- src/views/bingo.vue
+++ src/views/bingo.vue
@@ -57,17 +57,28 @@
 <template>
   <DocumentDisplay center>
     <VStack class="board" :space="0.5">
-      <HStack class="header row" :space="0.5">
-        <div v-for="letter in 'BINGO'" class="header cell second-layer">
+      <HStack class="row" :space="0.5">
+        <div
+          v-for="letter in 'BINGO'"
+          class="cell header second-layer text-color checked"
+        >
           {{ letter }}
         </div>
       </HStack>
 
-      <HStack v-for="row in board" class="row" :space="0.5">
+      <HStack v-for="(row, i) in board" class="row" :space="0.5">
         <button
-          v-for="cell in row"
+          v-for="(cell, j) in row"
           :key="cell.value"
-          :class="{ 'second-layer': cell.checked }"
+          :class="{
+            'second-layer': cell.checked,
+            'checked': cell.checked,
+            'lCheck': row[j - 1]?.checked,
+            'tCheck': board[i - 1]?.[j]?.checked,
+            'rCheck': row[j + 1]?.checked,
+            'bCheck': board[i + 1]?.[j]?.checked,
+            'tlCheck': board[i - 1]?.[j - 1]?.checked,
+          }"
           class="cell"
           @click="cell.checked = !cell.checked"
         >
@@ -78,7 +89,7 @@
   </DocumentDisplay>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
   .cell {
     display: flex;
     align-items: center;
@@ -90,5 +101,60 @@
     border: none;
     border-radius: 0.25rem;
     cursor: pointer;
+
+    &:not(.second-layer) {
+      background-color: transparent;
+    }
+  }
+
+  .checked.lCheck {
+    position: relative;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+
+    &::before {
+      position: absolute;
+      left: -0.5rem;
+      display: block;
+      width: 1rem;
+      height: 3rem;
+      background-color: var(--layer-background);
+      content: "";
+    }
+  }
+
+  .checked.tCheck {
+    position: relative;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+
+    &::after {
+      position: absolute;
+      top: -0.5rem;
+      display: block;
+      width: 3rem;
+      height: 1rem;
+      background-color: var(--layer-background);
+      content: "";
+    }
+
+    &.lCheck.tlCheck::after {
+      left: -0.5rem;
+      width: 3.5rem;
+    }
+  }
+
+  .checked.rCheck {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  .checked.bCheck {
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+
+  .header {
+    font-weight: bold;
   }
 </style>
