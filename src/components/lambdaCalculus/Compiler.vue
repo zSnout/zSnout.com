ac90e61ad60360db5d1d742f1d72d01d6a1ac424
Zachary <zsakowitz@zsnout.com>
2022-09-09 08:03:25 -0700

    Fix things

--- src/components/lambdaCalculus/Compiler.vue
+++ src/components/lambdaCalculus/Compiler.vue
@@ -1,20 +1,25 @@
 <script lang="ts" setup>
   import "ace-builds";
   import "ace-builds/src-noconflict/mode-javascript";
-  import { toRef } from "vue";
+  import { componentChars } from "mdurl/encode";
+  import { computed, toRef } from "vue";
   import { useCompileLambda } from "../../composables/useCompileLambda";
   import { toJS } from "../../composables/useEvaluateLambda";
   import AceEditor from "../AceEditor.vue";
 
   const props = defineProps<{ code: string; round?: boolean }>();
   const compiled = useCompileLambda(toRef(props, "code"));
+
+  const model = computed<string>({
+    get() {
+      return typeof compiled.value === "string"
+        ? compiled.value
+        : toJS(compiled.value);
+    },
+    set(v) {},
+  });
 </script>
 
 <template>
-  <AceEditor
-    :model-value="typeof compiled === 'string' ? compiled : toJS(compiled)"
-    :round="round"
-    mode="javascript"
-    readonly
-  />
+  <AceEditor v-model="model" :round="round" mode="javascript" readonly />
 </template>
