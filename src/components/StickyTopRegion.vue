77193e429a6e1b2ee034466dc2811872d5c5ff04
Zachary <zsakowitz@zsnout.com>
2022-09-10 08:09:11 -0700

    Improve StickyTopRegion

--- src/components/StickyTopRegion.vue
+++ src/components/StickyTopRegion.vue
@@ -10,7 +10,6 @@
     top: calc(3.5rem + 1px);
     width: calc(100% + 2rem);
     margin-top: -2rem !important;
-    margin-left: -1rem;
     padding: 0 1rem;
     padding-top: 2rem;
     padding-bottom: 2rem;
@@ -18,6 +17,10 @@
     font-size: 1.5em;
     -webkit-backdrop-filter: blur(0.5rem);
     backdrop-filter: blur(0.5rem);
+
+    &:not(.center &) {
+      margin-left: -1rem;
+    }
 
     @media (max-width: 400px) {
       top: calc(2.5rem + 1px);
