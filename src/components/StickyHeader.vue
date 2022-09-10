a2c4633d4507684a65008b80c5cbfa589402ffc5
Zachary <zsakowitz@zsnout.com>
2022-09-09 18:13:29 -0700

    Add StickyHeadert

---
+++ src/components/StickyHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <slot />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  header {
+    position: sticky;
+    top: calc(5.5em + 1px);
+
+    > :deep(:first-child) {
+      margin-top: 0;
+    }
+
+    > :deep(:last-child) {
+      margin-bottom: 0;
+    }
+
+    @media (max-width: 400px) {
+      top: calc(4em + 1px);
+    }
+  }
+</style>
