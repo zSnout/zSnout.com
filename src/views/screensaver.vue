f71a6dec1497929f5ef84ce715b2f8b380ab8cd4
Zachary <zsakowitz@zsnout.com>
2022-09-13 07:09:42 -0700

    Add screensaver.vue

---
+++ src/views/screensaver.vue
@@ -0,0 +1,86 @@
+<script lang="ts" setup>
+  import { useRafFn } from "@vueuse/core";
+  import { useIntervalFn } from "@vueuse/shared";
+  import images from "virtual:image-list";
+  import { reactive } from "vue";
+  import FullscreenDisplay from "../components/FullscreenDisplay.vue";
+  import { useRandomItem } from "../composables/useRandomItem";
+  import { appWidth } from "../main";
+
+  interface Image {
+    id: number;
+    size: number;
+    speed: number;
+    src: string;
+    x: number;
+    y: number;
+  }
+
+  const alive = reactive<Image[]>([]);
+  let lastSize: number = 0;
+
+  function generateSize(): number {
+    const size = Math.floor(3 * Math.random());
+
+    if (size === lastSize) {
+      return generateSize();
+    }
+
+    return (lastSize = size);
+  }
+
+  function createImage() {
+    const size = (generateSize() + 1) * 80;
+
+    alive.push({
+      id: Math.random(),
+      size,
+      speed: size / 160,
+      src: useRandomItem(images),
+      x: Math.floor(Math.random() * (innerWidth - size)),
+      y: innerHeight + 16,
+    });
+  }
+
+  function updateImages() {
+    for (const image of alive) {
+      if (image.y < -image.size - 16) {
+        alive.splice(alive.indexOf(image), 1);
+      }
+
+      image.y -= image.speed;
+    }
+  }
+
+  createImage();
+  useIntervalFn(createImage, () => (1920 / appWidth.value) * 1000);
+  useRafFn(updateImages);
+</script>
+
+<template>
+  <FullscreenDisplay>
+    <img
+      v-for="image in alive"
+      :key="image.id"
+      class="image shadow"
+      :src="image.src"
+      :style="{
+        left: `${image.x}px`,
+        top: `${image.y}px`,
+        width: `${image.size}px`,
+        height: `${image.size}px`,
+      }"
+    />
+  </FullscreenDisplay>
+</template>
+
+<style scoped>
+  #app {
+    overflow: hidden;
+  }
+
+  .image {
+    position: fixed;
+    background-color: white;
+  }
+</style>
