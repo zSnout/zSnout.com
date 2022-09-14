9131ef738a29d7033fa9a474b147227fcfd58b53
Zachary <zsakowitz@zsnout.com>
2022-09-14 07:26:00 -0700

    Add offline notification

---
+++ src/components/Notification.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+  defineProps<{ open: boolean }>();
+</script>
+
+<template>
+  <Transition appear name="notif">
+    <div v-if="open" class="notification second-layer">
+      <slot />
+    </div>
+  </Transition>
+</template>
+
+<style lang="scss" scoped>
+  .notification {
+    position: fixed;
+    bottom: 1rem;
+    left: calc(1rem + env(safe-area-inset-left, 0));
+    z-index: 8;
+    display: block;
+    width: min(300px, var(--app-width) - 2rem);
+    user-select: none;
+  }
+
+  .notif-enter-active {
+    transition: all 0.3s ease-out !important;
+  }
+
+  .notif-leave-active {
+    transition: all 0.3s ease-in !important;
+  }
+
+  .notif-enter-from,
+  .notif-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+  }
+</style>
