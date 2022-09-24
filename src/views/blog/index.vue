24236e48301c6d7a20ec28887b37fe35abb7b470
Zachary <zsakowitz@zsnout.com>
2022-09-24 15:33:02 -0700

    Change blog index layout

--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -2,12 +2,13 @@
   import { useMediaQuery } from "@vueuse/core";
   import articles from "virtual:blog-articles";
   import { ref } from "vue";
-  import Card from "../../components/Card.vue";
   import DocumentDisplay from "../../components/DocumentDisplay.vue";
   import InlineButton from "../../components/InlineButton.vue";
   import LargeTitle from "../../components/LargeTitle.vue";
   import LogInForm from "../../components/LogInForm.vue";
   import SearchableCardGrid from "../../components/SearchableCardGrid.vue";
+  import VStack from "../../components/VStack.vue";
+  import { isDark } from "../../composables/isDark";
   import { dateOf } from "../../composables/useDateOf";
   import {
     connected,
@@ -35,15 +36,53 @@
   const filter = ref<string>("");
   const isDesktop = useMediaQuery("(min-width: 1080px)");
   const open = ref(false);
+
+  const colors: Record<string, number> = {
+    code: 4,
+    math: 1,
+    meta: 5,
+    camp: 0,
+    saurs: 3,
+  };
+
+  function colorOf(label?: string) {
+    return colors[label || ""];
+  }
+
+  const lightColors = [
+    "#990000",
+    "#996300",
+    "#999900",
+    "#009900",
+    "#000099",
+    "#990099",
+  ];
+
+  const darkColors = [
+    "#ff6666",
+    "#ffc966",
+    "#ffff66",
+    "#66ff66",
+    "#8888ff",
+    "#ff66ff",
+  ];
+
+  function gradientFor(dark: boolean, category?: string) {
+    const pc = 75 * Math.random() + 12.5;
+    const color = (dark ? lightColors : darkColors)[colorOf(category)];
+
+    const bg = `linear-gradient(to right, transparent, transparent ${pc}%, ${color})`;
+    return `background: ${bg}`;
+  }
 </script>
 
 <template>
   <LogInForm v-model:open="open" />
 
-  <DocumentDisplay>
+  <DocumentDisplay small>
     <LargeTitle>Blog Articles</LargeTitle>
 
-    <p v-if="session">
+    <p v-if="session" class="text-color">
       You
       {{ willNotifyForBlog ? "are currently receiving" : "will not receive" }}
       notifications for updates to the zSnout blog.
@@ -65,27 +104,54 @@
       </template>
     </p>
 
-    <p v-else>
+    <p v-else class="text-color">
       <InlineButton @click="open = true">Log in</InlineButton>
       to a zSnout account or
       <InlineButton @click="open = true">sign up</InlineButton>
       to receive notification for new blog posts.
     </p>
 
-    <SearchableCardGrid :sizes="['list']" v-slot="{ size }">
+    <SearchableCardGrid :sizes="['list']">
       <template v-for="article in articles" :key="article.path">
-        <Card
+        <VStack
           v-if="!filter || article.frontmatter.category?.includes(filter)"
-          :date="article.frontmatter.date"
-          :description="article.frontmatter.excerpt || ''"
-          :label="article.frontmatter.category"
-          :title="
-            size === 'small'
-              ? dateOf(article.frontmatter.date)
-              : titleOf(article.path.slice(6, -3))
-          "
-          :to="article.path.slice(0, -3)"
-        />
+          class="card"
+          :space="0"
+          :style="gradientFor(isDark, article.frontmatter.category)"
+          :data-keywords="`${
+            article.frontmatter.author !== 'Zachary Sakowitz'
+              ? article.frontmatter.author
+              : ''
+          } ${article.frontmatter.keywords || ''} ${dateOf(
+            article.frontmatter.date
+          )}`"
+        >
+          <RouterLink class="no-link" :to="article.path.slice(0, -3)">
+            <p class="title">
+              {{ titleOf(article.path.slice(6, -3)) }}
+            </p>
+          </RouterLink>
+
+          <!-- category -->
+
+          <p
+            v-if="
+              article.frontmatter.author &&
+              article.frontmatter.author !== 'Zachary Sakowitz'
+            "
+            class="intro"
+          >
+            By {{ article.frontmatter.author }}
+          </p>
+
+          <p v-if="article.frontmatter.date" class="intro">
+            Published on {{ dateOf(article.frontmatter.date) }}
+          </p>
+
+          <p class="spacer" />
+
+          <p class="text-color">{{ article.frontmatter.excerpt }}</p>
+        </VStack>
       </template>
     </SearchableCardGrid>
 
@@ -122,3 +188,40 @@
     </template>
   </DocumentDisplay>
 </template>
+
+<style lang="scss" scoped>
+  .card {
+    padding: 0.5em 0;
+    padding-right: 0.5em;
+    border-top-right-radius: 0.5em;
+    border-bottom-right-radius: 0.5em;
+  }
+
+  .title {
+    margin: 0;
+    font-weight: 600;
+    font-size: 1.5em;
+  }
+
+  .intro {
+    margin-top: 0;
+    margin-bottom: 2em;
+    color: #404040;
+    font-weight: 600;
+    font-size: 0.875em;
+    line-height: 1.75em;
+
+    .dark & {
+      color: #c0c0c0;
+      font-weight: 500;
+    }
+
+    + .intro {
+      margin-top: -2em;
+    }
+  }
+
+  .spacer {
+    height: 0.5rem;
+  }
+</style>
