5a7df9aca2afe7cc6e37c4643e1e1e285410baad
Zachary <zsakowitz@zsnout.com>
2022-09-26 17:45:08 -0700

    Add WordLink

---
+++ src/components/WordLink.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  defineProps<{
+    comma?: boolean;
+    word: string;
+  }>();
+</script>
+
+<!-- prettier-ignore -->
+<template>
+  <a :href="`#${word}`">{{ word }}</a>{{ comma ? ", " : "" }}
+</template>
