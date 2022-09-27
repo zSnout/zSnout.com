839f875be0ce92d0befe80960fe3d124a5947135
Zachary <zsakowitz@zsnout.com>
2022-09-27 07:12:14 -0700

    Add isSource param to splitParagraph

--- src/views/language/index.ts
+++ src/views/language/index.ts
@@ -7,7 +7,19 @@ import("./words.yml?raw").then((doc) => {
   Object.assign(output, load(doc.default));
 });
 
-export function splitParagraph(text: string) {
+const wordRegex = /\w/;
+
+export function splitParagraph(text: string, isSource?: boolean) {
+  if (isSource) {
+    return (text.match(/\w+|[^\w]+/g) || []).map((text) => {
+      if (wordRegex.test(text)) {
+        return { word: text };
+      }
+
+      return { isText: true, word: text };
+    });
+  }
+
   return (text.match(/_\w+_|[^_]+/g) || []).map((text) => {
     if (text.startsWith("_")) {
       return { word: text.slice(1, -1) };
