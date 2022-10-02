ccbeb828c3bfc961b91b9df10117e739559e5a35
Zachary <zsakowitz@zsnout.com>
2022-10-01 18:12:06 -0700

    Have a limited number of modules

--- src/components/dashboard/Module.vue
+++ src/components/dashboard/Module.vue
@@ -5,13 +5,16 @@
   } as const;
 
   export type ModuleName = keyof typeof modules;
+
+  export const ids = [1, 2, 3, 4, 5, 6] as const;
+  export type Id = typeof ids extends readonly (infer T)[] ? T : never;
 </script>
 
 <script lang="ts" setup>
   import { useLocalStorage } from "@vueuse/core";
   import ScratchPad from "./ScratchPad.vue";
 
-  const { id } = defineProps<{ id: number }>();
+  const { id } = defineProps<{ id: Id }>();
   const module = useLocalStorage<ModuleName>(`module-${id}`, "");
 </script>
 
--- src/components/dashboard/ModulePicker.vue
+++ src/components/dashboard/ModulePicker.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
   import { useLocalStorage } from "@vueuse/core";
   import Dropdown from "../Dropdown.vue";
-  import { ModuleName, modules } from "./Module.vue";
+  import { Id, ModuleName, modules } from "./Module.vue";
 
-  const { id } = defineProps<{ id: number }>();
+  const { id } = defineProps<{ id: Id }>();
   const module = useLocalStorage<ModuleName>(`module-${id}`, "");
 </script>
 
