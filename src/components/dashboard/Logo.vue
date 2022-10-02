8556aeec77641b1fdd6ca380feb04e31c2442be8
Zachary <zsakowitz@zsnout.com>
2022-10-01 17:53:18 -0700

    Add dashboard logos

---
+++ src/components/dashboard/Logo.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+  import { isDark } from "../../composables/isDark";
+  import LogoDark from "./LogoDark.vue";
+  import LogoLight from "./LogoLight.vue";
+
+  withDefaults(defineProps<{ invert?: boolean }>(), { invert: false });
+</script>
+
+<template>
+  <LogoDark v-if="invert !== isDark" />
+  <LogoLight v-else />
+</template>
