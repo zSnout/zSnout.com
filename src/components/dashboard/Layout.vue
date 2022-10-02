f30b751e4187cdeca5810e36388200992a17380d
Zachary <zsakowitz@zsnout.com>
2022-10-02 08:28:55 -0700

    Fix some issues with Layout

--- src/components/dashboard/Layout.vue
+++ src/components/dashboard/Layout.vue
@@ -9,10 +9,10 @@
 </script>
 
 <template>
-  <VStack class="stack" no-center stretch v-bind="$attrs">
+  <VStack class="stack vstack" no-center stretch v-bind="$attrs">
     <Module v-if="layout === 'dualVert' || layout === 'single'" :id="1" />
 
-    <HStack v-else no-center stretch>
+    <HStack v-else :class="{ row: layout === 'quad' }" no-center stretch>
       <Module :id="1" />
 
       <Module :id="3" />
@@ -20,7 +20,7 @@
 
     <Module v-if="layout === 'dualVert'" :id="2" />
 
-    <HStack v-else-if="layout === 'quad'" no-center stretch>
+    <HStack v-else-if="layout === 'quad'" class="row" no-center stretch>
       <Module :id="2" />
 
       <Module :id="4" />
@@ -29,9 +29,21 @@
 </template>
 
 <style scoped>
+  :global(#app) {
+    overflow: hidden;
+  }
+
   .stack {
-    width: min(100%, 1600px);
     height: 100%;
     max-height: calc(var(--app-height) - 3.5em - 4em);
+  }
+
+  .row {
+    max-height: 50%;
+  }
+
+  .vstack {
+    width: min(100%, 1600px);
+    height: calc(var(--app-height) - 3.5em - 4em);
   }
 </style>
