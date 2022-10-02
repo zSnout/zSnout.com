2fb41f95930f9fa19c6d67d3e0876fbacbdd6b40
Zachary <zsakowitz@zsnout.com>
2022-10-01 18:10:24 -0700

    Add Module and ModulePicker

---
+++ src/components/dashboard/Module.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export const modules = {
+    "": "Blank Space",
+    "scratch-pad": "Scratch Pad",
+  } as const;
+
+  export type ModuleName = keyof typeof modules;
+</script>
+
+<script lang="ts" setup>
+  import { useLocalStorage } from "@vueuse/core";
+  import ScratchPad from "./ScratchPad.vue";
+
+  const { id } = defineProps<{ id: number }>();
+  const module = useLocalStorage<ModuleName>(`module-${id}`, "");
+</script>
+
+<template>
+  <ScratchPad v-if="module === 'scratch-pad'" />
+  <div v-else />
+</template>
---
+++ src/components/dashboard/ModulePicker.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  import { useLocalStorage } from "@vueuse/core";
+  import Dropdown from "../Dropdown.vue";
+  import { ModuleName, modules } from "./Module.vue";
+
+  const { id } = defineProps<{ id: number }>();
+  const module = useLocalStorage<ModuleName>(`module-${id}`, "");
+</script>
+
+<template>
+  <Dropdown v-model="module">
+    <option v-for="(name, value) in modules" :key="value" :value="value">
+      {{ name }}
+    </option>
+  </Dropdown>
+</template>
