2d503ff1c4db57fde26f85f3a7f173ffeaf3e645
Zachary <zsakowitz@zsnout.com>
2022-10-06 08:43:51 -0700

    Change useResolved

--- src/composables/useResolved.ts
+++ src/composables/useResolved.ts
@@ -4,7 +4,12 @@ export function useResolved<T>(promises: Iterable<T | PromiseLike<T>>) {
   const result: T[] = reactive([]);
 
   for (let item of promises) {
-    if ("then" in item && typeof item.then === "function") {
+    if (
+      typeof item === "object" &&
+      item != null &&
+      "then" in item &&
+      typeof item.then === "function"
+    ) {
       item.then((e) => result.push(e));
     } else result.push(item as any);
   }
