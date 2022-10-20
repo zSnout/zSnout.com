bbb1f67401dab7ecb440090a3b570b6b441079b4
Zachary <zsakowitz@zsnout.com>
2022-10-19 19:23:10 -0700

    Add TokiPonaText

---
+++ src/components/TokiPonaText.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+  const { content } = defineProps<{ content: string }>();
+
+  const matches = content.match(/_[^_]*_|[^_]*/);
+</script>
+
+<template>
+  <template v-for="match in matches">
+    <em v-if="match.startsWith('_')">{{ match.slice(1, -1) }}</em>
+    <span v-else>{{ match }}</span>
+  </template>
+</template>
