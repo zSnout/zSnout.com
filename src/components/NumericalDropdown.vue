40dcafce7c4e31675b8e244978e92e8372525f4a
Zachary <zsakowitz@zsnout.com>
2022-10-24 13:09:06 -0700

    Add NumericalDropdown

---
+++ src/components/NumericalDropdown.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  import Dropdown from "./Dropdown.vue";
+
+  defineProps<{ modelValue: number }>();
+  defineEmits<(event: "update:modelValue", value: number) => void>();
+</script>
+
+<template>
+  <Dropdown
+    :model-value="'' + modelValue"
+    @update:model-value="$emit('update:modelValue', +$event)"
+  >
+    <slot />
+  </Dropdown>
+</template>
