a3c6c601d8fbda02ebe40e8231e1e9aecedfbec3
zsakowitz <zsakowitz@users.noreply.github.com>
2022-11-17 02:08:10 +0000

    deploy: 76e96a34ea13f93e64629e8f6ed5ded47247a2b1

--- assets/Title.3fz2k97a.js
+++ assets/Title.3fz2k97a.js
@@ -1,1 +1,1 @@
-import{_ as t,o as _,h as s,r as c}from"./index.1ezwfhaa.js";const o={},n={class:"title"};function r(e,a){return _(),s("h1",n,[c(e.$slots,"default",{},void 0,!0)])}const d=t(o,[["render",r],["__scopeId","data-v-ce305be4"]]);export{d as T};
+import{_ as t,o as _,h as s,r as c}from"./index.5r9exxgm.js";const o={},n={class:"title"};function r(e,a){return _(),s("h1",n,[c(e.$slots,"default",{},void 0,!0)])}const d=t(o,[["render",r],["__scopeId","data-v-ce305be4"]]);export{d as T};
---
+++ assets/index.u5071e9h.js
@@ -0,0 +1,45 @@
+import{d as L,q as v,f as T,o as n,h as r,e as l,w as s,F as p,a as c,t as u,u as o,b as i,E as _,s as x,a5 as w,K as O,H as F,c as q,a6 as M,i as k,A as V,y as A,k as H,p as N,l as W,_ as Y}from"./index.5r9exxgm.js";import{D as R}from"./DocumentDisplay.633jhlro.js";import{I as m}from"./InlineButton.197m3pe8.js";import{L as E}from"./LargeTitle.1lwzjamx.js";import{_ as G}from"./BookmarkIcon.1thszj4g.js";import{S as J}from"./SearchableCardGrid.4qzy9rf8.js";import{V as U}from"./VStack.xad5wxbj.js";import{d as S}from"./useDateOf.86m05b59.js";import"./Navigation.vue_vue_type_script_setup_true_lang.5cehk67t.js";import"./Logo.vue_vue_type_script_setup_true_lang.5f5g8ttt.js";import"./LogoLight.539ztpd2.js";import"./SharedNav.28xyli12.js";import"./Title.3fz2k97a.js";import"./Field.2ydu2jt8.js";import"./CardGrid.2ocy45od.js";const b=[{path:"/blog/divisibility-rules.md",frontmatter:{excerpt:`
+You probably know how to find out if a number is divisible by 2, 5, or 10. But
+did you know that there's simple divisibility rules for other numbers? Let's
+check them out, from 2 to 12!`,author:"Zachary Sakowitz",category:"math",date:16389216e5}},{path:"/blog/does-0-equal-1.md",frontmatter:{excerpt:`
+The "0 = 1" paradox states that the numbers 0 and 1 are equivalent. We can
+easily prove this. Once done, we'll show you how you can use this to prove that
+any two numbers are equivalent!`,author:"Zachary Sakowitz",category:"math",date:1639008e6}},{path:"/blog/github-copilot-is-amazing.md",frontmatter:{excerpt:`
+GitHub Copilot is an amazing tool that can autocomplete code snippets for you.
+In this article I share my experience using GitHub Copilot: the upsides, the
+downsides, and what I think it could become.`,author:"Zachary Sakowitz",category:"code",date:16390944e5}},{path:"/blog/infinity-is-negative-one-twelfth.md",frontmatter:{excerpt:`
+Did you know that the sum of all whole numbers is equal to -1/12? In this
+article we find out how it's possible and prove it ourselves using simple
+algebra and math!`,author:"Zachary Sakowitz",category:"math",date:1639008e6}},{path:"/blog/learning-toki-pona.md",frontmatter:{excerpt:`
+Recently, I have been learning a language known as toki pona, also known as the
+language of good. toki pona's goal is to be a minimalistic language that still
+allows its speakers to communicate effectively. toki pona is one of the most
+well-known constructed language, or conlangs for short. It is estimated to have
+around 300 fluent speakers and over 10,000 people who can effectively read and
+understand it. My goal is to become one of those people who are fluent in the
+language. Today we'll explore my journey in learning toki pona and how you can
+learn it for yourself.`,author:"Zachary Sakowitz",date:16660512e5,category:"language"}},{path:"/blog/lets-make-a-web-framework.md",frontmatter:{excerpt:`
+Today we're going to make a reactivity system in JavaScript, harness it to
+create DOM constructs, and create a JSX framework using our new system.`,author:"Zachary Sakowitz",category:"code",date:1664064e6}},{path:"/blog/make-people-think-of-numbers.md",frontmatter:{excerpt:`
+Did you know that using math we can force people to think of certain numbers? We
+can use 9, 1089, or any other number you can think of using the simple tricks
+described below.`,author:"Zachary Sakowitz",category:"math",date:16389216e5}},{path:"/blog/mathemagical-tricks.md",frontmatter:{excerpt:`
+Have you ever wanted to convince your friend that you have psychic powers? In
+this article you'll learn how to use amazing mathematical principles to help you
+on your noble quest.`,author:"Zachary Sakowitz",category:"math",date:16490304e5}},{path:"/blog/meet-the-saurs.md",frontmatter:{excerpt:`
+Throughout my childhood, I've come up with many stories about a group of
+characters known as the Saurs. I'd like to share some of these stories with you
+so we can laugh about them together and so that they can be recorded in the
+oversized archives that are zSnout. This article is meant to be read in order,
+as each new topic links back to previous ones, but you can stop at any point and
+come back at a later date; there's a lot to take in. With that disclaimer, let's
+step into the technology-filled world of the Saurs!`,author:"Zachary Sakowitz",category:"saurs",date:16626816e5}},{path:"/blog/the-day-for-any-date.md",frontmatter:{excerpt:`
+Ever wondered what day your friend's birthday occurred, or whether the next July
+4th will be a Thursday? You can calculate it using simple math, which you'll
+learn here!`,author:"Zachary Sakowitz",category:"math",date:16389216e5}},{path:"/blog/the-outage-on-october-1.md",frontmatter:{excerpt:"",author:"Zachary Sakowitz",category:"meta",date:16648416e5}},{path:"/blog/the-story-of-zsnout.md",frontmatter:{excerpt:`
+Have you ever wondered how zSnout began? Let's hear the story together and take
+a deep dive into the early years of zSnout.`,author:"Zachary Sakowitz",category:"meta",date:16462656e5,updated:"2022-09-07T00:00:00.000Z"}},{path:"/blog/what-makes-zsnout-different.md",frontmatter:{excerpt:"Many things stand out about zSnout that aren't seen in other websites. Let's explore zSnout's purpose, theme system, and offline capabilities.",author:"Zachary Sakowitz",category:"meta",date:16390944e5,updated:"2022-09-04T00:00:00.000Z"}},{path:"/blog/yet-another-web-framework.md",frontmatter:{excerpt:`
+About 3 months ago, I published an article titled
+[Let's Make A Web Framework](/blog/lets-make-a-web-framework). While it was a
+fun article to write, I've been working on another reactive system that I think
+is better, and I want to share it with you. Let's dive in!`,author:"Zachary Sakowitz",category:"code",date:1668384e6}}],j=f=>(N("data-v-168f1764"),f=f(),W(),f),K={key:0,class:"text-color"},P={key:1,class:"text-color"},Q={class:"title"},X={key:0,class:"intro"},tt={key:1,class:"intro"},et={key:2,class:"spacer"},ot={class:"text-color"},at=j(()=>c("p",null,[c("strong",null,"Filter By:")],-1)),nt=L({__name:"index",setup(f){const z=b.map(a=>a.frontmatter.category).filter(a=>!!a).filter((a,e,d)=>d.indexOf(a)===e);b.sort((a,e)=>{if(!a.frontmatter.date)return 1;if(!e.frontmatter.date)return-1;const d=new Date(a.frontmatter.date);return new Date(e.frontmatter.date).valueOf()-d.valueOf()});const h=v(""),I=T("(min-width: 1080px)"),y=v(!1),Z={camp:0,code:4,language:2,math:1,meta:5,saurs:3};function C(a){return Z[a||""]}const B=["#990000","#996300","#999900","#009900","#000099","#990099"],$=["#ff6666","#ffc966","#f0f060","#66ff66","#8888ff","#ff66ff"];function D(a,e){const d=75*Math.random()+12.5,t=(a?B:$)[C(e)];return`background: ${`linear-gradient(to right, transparent, transparent ${d}%, ${t})`}`}return(a,e)=>{const d=H("RouterLink");return n(),r(p,null,[l(G,{open:y.value,"onUpdate:open":e[0]||(e[0]=t=>y.value=t)},null,8,["open"]),l(R,{small:""},{help:s(()=>[c("p",null," You can "+u(o(I)?"use the sidebar to filter the pages.":"use the table of contents to filter the pages."),1)]),aside:s(()=>[at,c("nav",null,[c("ul",null,[c("li",null,[l(m,{onClick:e[4]||(e[4]=t=>h.value="")},{default:s(()=>[i("Reset Filter")]),_:1})]),(n(!0),r(p,null,_(o(z),t=>(n(),r("li",{key:t},[l(m,{active:h.value===t,onClick:g=>h.value===t?h.value="":h.value=t},{default:s(()=>[i(u(t[0].toUpperCase()+t.slice(1)),1)]),_:2},1032,["active","onClick"])]))),128))])])]),default:s(()=>[l(E,null,{default:s(()=>[i("Blog Articles")]),_:1}),o(x)?(n(),r("p",K,[i(" You "+u(o(w)?"are currently receiving":"will not receive")+" notifications for updates to the zSnout blog. ",1),o(O)?(n(),r(p,{key:0},[i(" Click "),l(m,{onClick:e[1]||(e[1]=t=>o(F).emit("blog:update:will-notify",o(x),!o(w)))},{default:s(()=>[i(" here ")]),_:1}),i(" to opt-"+u(o(w)?"out of":"in to")+" notifications. ",1)],64)):(n(),r(p,{key:1},[i(" You cannot change notification settings when offline. ")],64))])):(n(),r("p",P,[l(m,{onClick:e[2]||(e[2]=t=>y.value=!0)},{default:s(()=>[i("Log in")]),_:1}),i(" to a zSnout account or "),l(m,{onClick:e[3]||(e[3]=t=>y.value=!0)},{default:s(()=>[i("sign up")]),_:1}),i(" to receive notification for new blog posts. ")])),l(J,{sizes:["list"]},{default:s(()=>[(n(!0),r(p,null,_(o(b),t=>{var g;return n(),r(p,{key:t.path},[!h.value||((g=t.frontmatter.category)==null?void 0:g.includes(h.value))?(n(),q(U,{key:0,class:"card",space:0,style:V(D(o(A),t.frontmatter.category)),"data-keywords":`${t.frontmatter.author!=="Zachary Sakowitz"?t.frontmatter.author:""} ${t.frontmatter.keywords||""} ${o(S)(t.frontmatter.date)}`},{default:s(()=>[l(d,{class:"no-link",to:t.path.slice(0,-3)},{default:s(()=>[c("p",Q,u(o(M)(t.path.slice(6,-3))),1)]),_:2},1032,["to"]),t.frontmatter.author&&t.frontmatter.author!=="Zachary Sakowitz"?(n(),r("p",X," By "+u(t.frontmatter.author),1)):k("",!0),t.frontmatter.date?(n(),r("p",tt," Published on "+u(o(S)(t.frontmatter.date)),1)):k("",!0),t.frontmatter.excerpt?(n(),r("p",et)):k("",!0),c("p",ot,u(t.frontmatter.excerpt),1)]),_:2},1032,["style","data-keywords"])):k("",!0)],64)}),128))]),_:1})]),_:1})],64)}}});const bt=Y(nt,[["__scopeId","data-v-168f1764"]]);export{bt as default};
